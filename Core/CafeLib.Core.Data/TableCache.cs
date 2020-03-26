e9bed2a0977dc5b57c876405a6591031a9b16a0b
chrissolutions <chris@chrissolutions.com>
2020-03-27 10:37:35 +1300

    Fixed TableCache to construct table names from domain.

--- Core/CafeLib.Core.Data/TableCache.cs
+++ Core/CafeLib.Core.Data/TableCache.cs
@@ -3,6 +3,8 @@ using System.Collections.Concurrent;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 using System.Reflection;
+using CafeLib.Core.Extensions;
+
 // ReSharper disable UnusedMember.Global
 
 namespace CafeLib.Core.Data
@@ -13,59 +15,37 @@ namespace CafeLib.Core.Data
     // ReSharper disable once UnusedMember.Global
     internal class TableCache
     {
-        private static string _prefix = string.Empty;
-        private static string _suffix = string.Empty;
+        private readonly Domain _domain;
 
-        private readonly ConcurrentDictionary<Type, string> _tableNames;
+        private readonly ConcurrentDictionary<Type, string> _tableNames = new ConcurrentDictionary<Type, string>();
 
         public TableCache(Domain domain)
         {
-            _tableNames = new ConcurrentDictionary<Type, string>();
+            _domain = domain;
+            SetupConvention(string.Empty, string.Empty);
         }
 
         /// <summary>
         /// Used to setup custom table conventions.
         /// </summary>
-        /// <param name="tablePrefix">table name prefix</param>
-        /// <param name="tableSuffix">table name suffix</param>
-        // ReSharper disable once UnusedMember.Global
-        public void SetupConvention(string tablePrefix, string tableSuffix)
+        /// <param name="prefix">table name prefix</param>
+        /// <param name="suffix">table name suffix</param>
+        public void SetupConvention(string prefix, string suffix)
         {
-            if (_tableNames.Count > 0)
-            {
-                throw new InvalidConstraintException("TableMapper.SetupConvention called after usage.");
-            }
-
-            _prefix = tablePrefix;
-            _suffix = tableSuffix;
-
             _tableNames.Clear();
+            _domain.GetEntityTypes().ForEach(x =>
+            {
+                var tableAttr = x.GetCustomAttribute<TableAttribute>(false);
+                var name = tableAttr switch
+                {
+                    _ when tableAttr != null => tableAttr.Name,
+                    _ => $"{prefix}{(x.IsInterface && x.Name.StartsWith("I") ? x.Name.Substring(1) : x.Name)}{suffix}"
+                };
+                _tableNames[x] = name;
+            });
         }
 
         public string TableName<T>() where T : IEntity => TableName(typeof(T));
-
-        public string TableName(Type type)
-        {
-            if (_tableNames.TryGetValue(type, out var name))
-            {
-                return name;
-            }
-
-            var tableAttr = type.GetCustomAttribute<TableAttribute>(false);
-            if (tableAttr != null)
-            {
-                name = tableAttr.Name;
-            }
-            else
-            {
-                name = type.IsInterface && type.Name.StartsWith("I")
-                    ? type.Name.Substring(1)
-                    : type.Name;
-                name = $"{_prefix}{name}{_suffix}";
-            }
-
-            _tableNames[type] = name;
-            return name;
-        }
+        public string TableName(Type type) => _tableNames[type];
     }
 }
