3c0e2db8558fc713ca84145502f9538d5c31c5fa
chrissolutions <chris@chrissolutions.com>
2020-03-27 10:47:31 +1300

    Added CafeLib.Core.Data to nugetinstall

--- Core/nugetinstall.cmd
+++ Core/nugetinstall.cmd
@@ -17,6 +17,7 @@ set sourcepath=C:\Projects\ChrisSolutions\CafeLib\%lib%
 @echo on
 %nuget% push %sourcepath%\CafeLib.Core\%libPath%\CafeLib.Core.%version%.nupkg %apikey% -source %nugetRepo%
 %nuget% push %sourcepath%\CafeLib.Core.Caching\%libPath%\CafeLib.Core.Caching.%version%.nupkg %apikey% -source %nugetRepo%
+%nuget% push %sourcepath%\CafeLib.Core.Data\%libPath%\CafeLib.Core.Data.%version%.nupkg %apikey% -source %nugetRepo%
 %nuget% push %sourcepath%\CafeLib.Core.Eventing\%libPath%\CafeLib.Core.Eventing.%version%.nupkg %apikey% -source %nugetRepo%
 %nuget% push %sourcepath%\CafeLib.Core.IoC\%libPath%\CafeLib.Core.IoC.%version%.nupkg %apikey% -source %nugetRepo%
 %nuget% push %sourcepath%\CafeLib.Core.Logging\%libPath%\CafeLib.Core.Logging.%version%.nupkg %apikey% -source %nugetRepo%
