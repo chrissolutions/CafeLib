3707d50af500f8bb5d86b46446b33a51bfd71d44
chrissolutions <chris@chrissolutions.com>
2020-05-26 12:05:43 +1200

    Renamed AsyncLazy to LazyAsync

--- Core/CafeLib.Core/Support/LazyAsync.cs
+++ Core/CafeLib.Core/Support/LazyAsync.cs
@@ -3,14 +3,14 @@ using System.Threading.Tasks;
 
 namespace CafeLib.Core.Support
 {
-    public class AsyncLazy<T> : Lazy<Task<T>>
+    public class LazyAsync<T> : Lazy<Task<T>>
     {
-        public AsyncLazy(Func<T> valueFactory) :
+        public LazyAsync(Func<T> valueFactory) :
             base(() => Task.Factory.StartNew(valueFactory))
         {
         }
 
-        public AsyncLazy(Func<Task<T>> taskFactory) :
+        public LazyAsync(Func<Task<T>> taskFactory) :
             base(async () => await taskFactory().ConfigureAwait(false))
         {
         }
