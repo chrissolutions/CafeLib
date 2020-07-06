81243d45ad9fce81cdeea6dca3fd7d3de72a6381
chrissolutions <chris@chrissolutions.com>
2020-07-06 16:00:30 +1200

    Modified GetRequestTest to use httpbin.org

--- Network/CafeLib.Web.Request.UnitTests/WebRequestTest.cs
+++ Network/CafeLib.Web.Request.UnitTests/WebRequestTest.cs
@@ -11,9 +11,13 @@ namespace CafeLib.Web.Request.UnitTests
         [Fact]
         public async void GetRequestTest()
         {
-            var request = new WebRequest<JToken>("https://api.github.com/users/mralexgray/repos");
+            const string endpoint = "https://httpbin.org/anything";
+            var request = new WebRequest<JToken>(endpoint);
             var json = await request.GetAsync();
             Assert.NotNull(json);
+            var result = json.ToObject<BinResult>();
+            Assert.Equal("GET", result.Method);
+            Assert.Equal(endpoint, result.Url);
         }
 
         [Fact]
