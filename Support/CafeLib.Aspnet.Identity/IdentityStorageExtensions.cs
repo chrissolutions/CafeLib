65360dd34042dccbc8005580c6f800cd93de2665
chrissolutions <chris@chrissolutions.com>
2020-07-19 12:38:24 +1200

    Fixed calls to FindUserByEmailAddress & FindUserByUserName.

--- Support/CafeLib.Aspnet.Identity/IdentityStorageExtensions.cs
+++ Support/CafeLib.Aspnet.Identity/IdentityStorageExtensions.cs
@@ -8,12 +8,14 @@ namespace CafeLib.Aspnet.Identity
     {
         public static async Task<T> FindUserByEmailAddress<T>(this IdentityStorage storage, string emailAddress) where T : IdentityUser
         {
-            return await storage.FindOne<T>(x => x.NormalizedEmail == emailAddress);
+            var normalizedEmail = emailAddress.ToUpper(CultureInfo.CurrentCulture);
+            return await storage.FindOne<T>(x => x.NormalizedEmail == normalizedEmail);
         }
 
         public static async Task<T> FindUserByUserName<T>(this IdentityStorage storage, string userName) where T : IdentityUser
         {
-            return await storage.FindOne<T>(x => x.NormalizedUserName.ToLower(CultureInfo.CurrentCulture) == userName.ToLower(CultureInfo.CurrentCulture));
+            var normalizedUserName = userName.ToUpper(CultureInfo.CurrentCulture);
+            return await storage.FindOne<T>(x => x.NormalizedUserName == normalizedUserName);
         }
     }
 }
