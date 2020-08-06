3f89504e1bed4c6d38848fd421db6b000bdcd7bd
chrissolutions <chris@chrissolutions.com>
2020-08-06 18:05:40 +1200

    Removed CafeLib.Support.

--- Support/CafeLib.Aspnet.Identity.Secrets/CafeLib.Aspnet.Identity.Secrets/IPasswordHash.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CafeLib.Aspnet.Identity.Secrets
-{
-	public interface IPasswordHash
-	{		
-		string HashPassword(string password);
-		bool VerifyHashedPassword(string hashedPassword, string providedPassword);
-	}
-}
