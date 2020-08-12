94eba58f8570ff6b4709731f653f12db1791a2d1
chrissolutions <chris@chrissolutions.com>
2020-08-13 10:10:48 +1200

    Reworked DateTimeExtensions.Truncate to TruncateXXX

--- Core/CafeLib.Core/Extensions/DateTimeExtensions.cs
+++ Core/CafeLib.Core/Extensions/DateTimeExtensions.cs
@@ -7,6 +7,38 @@ namespace CafeLib.Core.Extensions
     public static class DateTimeExtensions
     {
         /// <summary>
+        /// Adds interval to datetime until greater than current time.
+        /// </summary>
+        /// <param name="dateTime">date time</param>
+        /// <param name="interval">interval time span</param>
+        /// <returns>datetime</returns>
+        public static DateTime NextTime(this DateTime dateTime, TimeSpan interval)
+        {
+            var timespan = dateTime < DateTime.Now
+                ? interval != TimeSpan.Zero ? interval : throw new ArgumentException($"Zero {nameof(interval)} results in endless loop.")
+                : TimeSpan.Zero;
+
+            var nextTime = dateTime.Add(timespan);
+            while (nextTime < DateTime.Now)
+            {
+                nextTime = nextTime.Add(timespan);
+            }
+
+            return nextTime;
+        }
+
+        /// <summary>
+        /// Converts datetime to Unix time.
+        /// </summary>
+        /// <param name="dateTime">date time</param>
+        /// <returns></returns>
+        public static long ToUnixTime(this DateTime dateTime)
+        {
+            var sTime = DateTime.UnixEpoch;
+            return (long) (dateTime - sTime).TotalSeconds;
+        }
+
+        /// <summary>
         /// Converts a date time to universal time string.
         /// </summary>
         /// <param name="dateTime">date time</param>
@@ -19,40 +51,55 @@ namespace CafeLib.Core.Extensions
         }
 
         /// <summary>
-        /// Truncate datetime.
+        /// Truncates hours from datetime.
         /// </summary>
-        /// <param name="dateTime">datetime</param>
-        /// <param name="timeSpan">the portion of datetime to truncate</param>
-        /// <returns>truncated datetime</returns>
-        public static DateTime Truncate(this DateTime dateTime, TimeSpan timeSpan = default)
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime TruncateHours(this DateTime dateTime)
         {
-            return dateTime switch
-            {
-                _ when timeSpan == default => dateTime,
-                _ when dateTime == DateTime.MinValue || dateTime == DateTime.MaxValue => dateTime,
-                _ => dateTime.AddTicks(-(dateTime.Ticks % timeSpan.Ticks))
-            };
+            return dateTime.Truncate(TimeSpan.TicksPerDay);
         }
 
         /// <summary>
-        /// Adds interval to datetime until greater than current time.
+        /// Truncates minutes from datetime.
         /// </summary>
-        /// <param name="dateTime">date time</param>
-        /// <param name="interval">interval time span</param>
-        /// <returns>datetime</returns>
-        public static DateTime NextTime(this DateTime dateTime, TimeSpan interval)
+        /// <param name="dateTime">datetime source.</param>
+        /// <returns>truncated datetime</returns>
+        public static DateTime TruncateMinutes(this DateTime dateTime)
         {
-            var timespan = dateTime < DateTime.Now 
-                ? interval != TimeSpan.Zero ? interval : throw new ArgumentException($"Zero {nameof(interval)} results in endless loop.")
-                : TimeSpan.Zero;
+            return dateTime.Truncate(TimeSpan.TicksPerHour);
+        }
 
-            var nextTime = dateTime.Add(timespan);
-            while (nextTime < DateTime.Now)
-            {
-                nextTime = nextTime.Add(timespan);
-            }
+        /// <summary>
+        /// Truncates seconds from dateTime.
+        /// </summary>
+        /// <param name="dateTime">datetime source.</param>
+        /// <returns>truncated datetime</returns>
+        public static DateTime TruncateSeconds(this DateTime dateTime)
+        {
+            return dateTime.Truncate(TimeSpan.TicksPerMinute);
+        }
 
-            return nextTime;
+        /// <summary>
+        /// Truncates milliseconds from dateTime.
+        /// </summary>
+        /// <param name="dateTime">datetime source.</param>
+        /// <returns>truncated datetime</returns>
+        public static DateTime TruncateMilliseconds(this DateTime dateTime)
+        {
+            return dateTime.Truncate(TimeSpan.TicksPerSecond);
+        }
+
+        /// <summary>
+        /// Truncates a DateTime to a specified resolution.
+        /// A convenient source for resolution is TimeSpan.TicksPerXXXX constants.
+        /// </summary>
+        /// <param name="date">The DateTime object to truncate</param>
+        /// <param name="resolution">e.g. to round to nearest second, TimeSpan.TicksPerSecond</param>
+        /// <returns>Truncated DateTime</returns>
+        private static DateTime Truncate(this DateTime date, long resolution)
+        {
+            return new DateTime(date.Ticks - (date.Ticks % resolution), date.Kind);
         }
     }
 }
