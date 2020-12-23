fe957f374270c123b30b8bc91ba7cfd2286742c4
chrissolutions <chris@chrissolutions.com>
2020-12-23 16:20:15 +1300

    Removed async from RunnerBase.Start method to return task.

--- Core/CafeLib.Core.Runnable/RunnerBase.cs
+++ Core/CafeLib.Core.Runnable/RunnerBase.cs
@@ -77,7 +77,8 @@ namespace CafeLib.Core.Runnable
         /// <summary>
         /// Start the service.
         /// </summary>
-        public virtual async Task Start()
+        /// <returns>start task</returns>
+        public virtual Task Start()
         {
             if (!IsRunning)
             {
@@ -85,7 +86,8 @@ namespace CafeLib.Core.Runnable
                 OnAdvise(new RunnerEventMessage(ErrorLevel.Ignore, $"{Name} started."));
                 BeginLoop();
             }
-            await Task.CompletedTask;
+
+            return Task.CompletedTask;
         }
 
         /// <summary>
