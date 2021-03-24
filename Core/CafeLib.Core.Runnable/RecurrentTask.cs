5c3154f3577f7c319f1e7433b07552d6f8129e89
chrissolutions <chris@chrissolutions.com>
2021-03-25 09:15:09 +1300

    Added use of AsyncLocal in RecurrentTask.

--- Core/CafeLib.Core.Runnable/RecurrentTask.cs
+++ Core/CafeLib.Core.Runnable/RecurrentTask.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using CafeLib.Core.Extensions;
+
 // ReSharper disable UnusedMember.Global
 
 namespace CafeLib.Core.Runnable
 {
     public class RecurrentTask : RunnerBase
     {
-        private readonly Func<Task> _task;
-        private readonly TimeSpan _interval;
-        private DateTime _triggerTime;
+        private readonly AsyncLocal<Func<Task>> _task;
+        private readonly AsyncLocal<TimeSpan> _interval;
+        private readonly AsyncLocal<DateTime> _triggerTime;
+
+        private Func<Task> Task => _task.Value;
+
+        private TimeSpan Interval => _interval.Value;
+
+        private DateTime TriggerTime
+        {
+            get => _triggerTime.Value;
+            set => _triggerTime.Value = value;
+        }
 
         /// <summary>
         /// RecurrentTask constructor.
@@ -19,7 +31,7 @@ namespace CafeLib.Core.Runnable
         /// <param name="startTime">start time</param>
         /// <param name="frequency">frequency in milliseconds (default is 1 second)</param>
         public RecurrentTask(Func<Task> task, int interval, DateTime startTime = default, int frequency = 1000)
-            : this(task, TimeSpan.FromMilliseconds(interval), startTime, frequency)
+                : this(task, TimeSpan.FromMilliseconds(interval), startTime, frequency)
         {
         }
 
@@ -45,9 +57,9 @@ namespace CafeLib.Core.Runnable
         public RecurrentTask(Func<Task> task, TimeSpan interval, DateTime startTime = default, int frequency = 1000)
             : base(frequency)
         {
-            _task = task ?? (() => Task.CompletedTask);
-            _interval = interval;
-            _triggerTime = GetTriggerTime(startTime);
+            _task = new AsyncLocal<Func<Task>> { Value = task ?? (() => System.Threading.Tasks.Task.CompletedTask) };
+            _interval = new AsyncLocal<TimeSpan> { Value = interval };
+            _triggerTime = new AsyncLocal<DateTime> { Value = GetTriggerTime(startTime) };
         }
 
         /// <summary>
@@ -56,10 +68,10 @@ namespace CafeLib.Core.Runnable
         /// <returns>asynchronous task</returns>
         protected override async Task Run()
         {
-            if (DateTime.Now >= _triggerTime)
+            if (DateTime.Now >= TriggerTime)
             {
-                await _task();
-                _triggerTime = GetTriggerTime(_triggerTime);
+                await Task();
+                TriggerTime = GetTriggerTime(TriggerTime);
             }
         }
 
@@ -70,7 +82,7 @@ namespace CafeLib.Core.Runnable
         /// <returns></returns>
         private DateTime GetTriggerTime(DateTime startTime)
         {
-            return startTime == default ? DateTime.Now : startTime.NextTime(_interval);
+            return startTime == default ? DateTime.Now : startTime.NextTime(Interval);
         }
     }
 }
