0d3542232a870a320aea8a0fa3e75a0e37168bc0
chrissolutions <chris@chrissolutions.com>
2021-04-01 10:02:05 +1300

    Added empty bitcoin & unit test projects.

---
+++ Enterprise/Bitcoin/CafeLib.Bitcoin/CafeLib.Bitcoin.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CafeLib.Bitcoin.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
