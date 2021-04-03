80d6f39168f4b32a030ef88dd57878999df4d9f0
chrissolutions <chris@chrissolutions.com>
2021-04-04 11:13:42 +1200

    Update nugetinstall script, added help and changed parameters.

--- Core/nugetinstall.cmd
+++ Core/nugetinstall.cmd
@@ -1,11 +1,34 @@
 @echo off
 setlocal
 
-if '%1' == '' echo missing version && goto exit
-
-:: Configuration
+:: Library
 set lib=Core
-set version=%1
+
+:: Settings
+set msbld=msbuild.exe
+set nuget=nuget.exe
+set configuration=Debug
+set libPath=bin\%configuration%
+set apikey=
+set nugetRepo=C:\Nuget\repo
+set sourcepath=C:\Projects\ChrisSolutions\CafeLib\%lib%
+set solution=CafeLib.%lib%
+set version=
+
+:: Parse arguments
+if '%1' == '' goto usage
+:nextarg
+set arg=%1
+if '%arg%' == '' goto start
+if '%arg%' == '-v' set version=%2&&shift&&shift&&goto nextarg
+if '%arg%' == '/v' set version=%2&&shift&&shift&&goto nextarg
+if '%arg%' == '-c' set configuration=%2&&shift&&shift&&goto nextarg
+if '%arg%' == '/c' set configuration=%1&&shift&&shift&&goto nextarg
+goto usage
+
+:start
+if '%version' == '' goto usage
+if '%configuration' == '' goto usage
 
 :: Settings
 set msbld=msbuild.exe
@@ -33,14 +56,15 @@ set libs=%libs% %solution%.Queueing
 set libs=%libs% %solution%.Runnable
 
 echo Create Nuget Package for %solution% ...
-@echo on
-for %%X in (%libs%) DO %msbld% %sourcepath%\%%X\%%X.csproj -t:pack -p:PackageVersion=%version%
-@echo off
+for %%X in (%libs%) DO @echo on&&%msbld% %sourcepath%\%%X\%%X.csproj -t:pack -p:PackageVersion=%version% -p:Configuration=%configuration%&&@echo off
 
 echo Push Package to Nuget repository ...
-@echo on
-for %%X in (%libs%) DO %nuget% push %sourcepath%\%%X\%libPath%\%%X.%version%.nupkg %apikey% -source %nugetRepo%
-@echo off
+for %%X in (%libs%) DO @echo on&&%nuget% push %sourcepath%\%%X\%libPath%\%%X.%version%.nupkg %apikey% -source %nugetRepo%&&@echo off
+goto exit
+
+:usage
+echo nugetinstall -v ^<version number^> [-c ^<configuration^> Debug is default]
+goto exit
 
 :exit
 endlocal
