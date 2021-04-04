27ac5be7317745f4ff75d7e7a2b185a7eca3f42b
chrissolutions <chris@chrissolutions.com>
2021-04-04 12:10:56 +1200

    Modified nugetinstall for CafeLib.Data

--- Data/nugetinstall.cmd
+++ Data/nugetinstall.cmd
@@ -1,11 +1,8 @@
 @echo off
 setlocal
 
-if '%1' == '' echo missing version && goto exit
-
-:: Configuration
+:: Library
 set lib=Data
-set version=%1
 
 :: Settings
 set msbld=msbuild.exe
@@ -16,6 +13,22 @@ set apikey=
 set nugetRepo=C:\Nuget\repo
 set sourcepath=C:\Projects\ChrisSolutions\CafeLib\%lib%
 set solution=CafeLib.%lib%
+set version=
+
+:: Parse arguments
+if '%1' == '' goto usage
+:nextarg
+set arg=%1
+if '%arg%' == '' goto start
+if '%arg%' == '-v' set version=%2&&shift&&shift&&goto nextarg
+if '%arg%' == '/v' set version=%2&&shift&&shift&&goto nextarg
+if '%arg%' == '-c' set configuration=%2&&shift&&shift&&goto nextarg
+if '%arg%' == '/c' set configuration=%1&&shift&&shift&&goto nextarg
+goto usage
+
+:start
+if '%version' == '' goto usage
+if '%configuration' == '' goto usage
 
 set libs=%solution%
 set libs=%libs% %solution%.Mapping 
@@ -25,14 +38,15 @@ set libs=%libs% %solution%.Sources.Sqlite
 set libs=%libs% %solution%.Sources.SqlServer
 
 echo Create Nuget Package for %solution% ...
-@echo on
-for %%X in (%libs%) DO %msbld% %sourcepath%\%%X\%%X.csproj -t:pack -p:PackageVersion=%version%
-@echo off
+for %%X in (%libs%) DO @echo on&&%msbld% %sourcepath%\%%X\%%X.csproj -t:pack -p:PackageVersion=%version% -p:Configuration=%configuration%&&@echo off
 
 echo Push Package to Nuget repository ...
-@echo on
-for %%X in (%libs%) DO %nuget% push %sourcepath%\%%X\%libPath%\%%X.%version%.nupkg %apikey% -source %nugetRepo%
-@echo off
+for %%X in (%libs%) DO @echo on&&%nuget% push %sourcepath%\%%X\%libPath%\%%X.%version%.nupkg %apikey% -source %nugetRepo%&&@echo off
+goto exit
+
+:usage
+echo nugetinstall -v ^<version number^> [-c ^<configuration^> Debug is default]
+goto exit
 
 :exit
 endlocal
