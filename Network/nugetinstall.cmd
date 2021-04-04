eb573832e08b9d688508d43c4632c27b87be484b
chrissolutions <chris@chrissolutions.com>
2021-04-04 12:25:32 +1200

    Modified nugetinstall for CafeLib.Network

--- Network/nugetinstall.cmd
+++ Network/nugetinstall.cmd
@@ -1,11 +1,8 @@
 @echo off
 setlocal
 
-if '%1' == '' echo missing version && goto exit
-
-:: Configuration
+:: Library
 set lib=Network
-set version=%1
 
 :: Settings
 set msbld=msbuild.exe
@@ -15,21 +12,38 @@ set libPath=bin\%configuration%
 set apikey=
 set nugetRepo=C:\Nuget\repo
 set sourcepath=C:\Projects\ChrisSolutions\CafeLib\%lib%
+set version=
+
+:: Parse arguments
+if '%1' == '' goto usage
+:nextarg
+set arg=%1
+if '%arg%' == '' goto start
+if '%arg%' == '-v' set version=%2&&shift&&shift&&goto nextarg
+if '%arg%' == '/v' set version=%2&&shift&&shift&&goto nextarg
+if '%arg%' == '-c' set configuration=%2&&shift&&shift&&goto nextarg
+if '%arg%' == '/c' set configuration=%1&&shift&&shift&&goto nextarg
+goto usage
+
+:start
+if '%version' == '' goto usage
+if '%configuration' == '' goto usage
 
 set libs=CafeLib.AspNet.WebSockets
 set libs=%libs% CafeLib.Web.Request 
 set libs=%libs% CafeLib.Web.SignalR
 set libs=%libs% CafeLib.Web.SignalR.Hubs 
 
-echo Create Nuget Package for %lib% ...
-@echo on
-for %%X in (%libs%) DO %msbld% %sourcepath%\%%X\%%X.csproj -t:pack -p:PackageVersion=%version%
-@echo off
+echo Create Nuget Package for %solution% ...
+for %%X in (%libs%) DO @echo on&&%msbld% %sourcepath%\%%X\%%X.csproj -t:pack -p:PackageVersion=%version% -p:Configuration=%configuration%&&@echo off
 
 echo Push Package to Nuget repository ...
-@echo on
-for %%X in (%libs%) DO %nuget% push %sourcepath%\%%X\%libPath%\%%X.%version%.nupkg %apikey% -source %nugetRepo%
-@echo off
+for %%X in (%libs%) DO @echo on&&%nuget% push %sourcepath%\%%X\%libPath%\%%X.%version%.nupkg %apikey% -source %nugetRepo%&&@echo off
+goto exit
+
+:usage
+echo nugetinstall -v ^<version number^> [-c ^<configuration^> Debug is default]
+goto exit
 
 :exit
 endlocal
