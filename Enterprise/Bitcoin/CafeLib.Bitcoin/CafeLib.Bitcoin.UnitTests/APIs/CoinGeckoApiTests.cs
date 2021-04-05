044acb2ab3fc937a1583ee43e9e5515d6a8fa87b
chrissolutions <chris@chrissolutions.com>
2021-04-05 15:24:24 +1200

    Added test to locate BSV supported coin.

--- Enterprise/Bitcoin/CafeLib.Bitcoin/CafeLib.Bitcoin.UnitTests/APIs/CoinGeckoApiTests.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin/CafeLib.Bitcoin.UnitTests/APIs/CoinGeckoApiTests.cs
@@ -3,19 +3,32 @@
 // Distributed under the Open BSV software license, see the accompanying file LICENSE.
 #endregion
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using CafeLib.Bitcoin.Api.CoinGecko;
 using Xunit;
 
 namespace CafeLib.Bitcoin.UnitTests.APIs {
-    public class CoinGeckoApiTests {
+    public class CoinGeckoApiTests
+    {
         [Fact]
-        public async Task GetSupportedCoinsTest() {
+        public async Task GetSupportedCoins_Test() 
+        {
             var api = new CoinGecko();
             var coins = await api.GetSupportedCoins();
             Assert.NotNull(coins);
             Assert.True(coins.Any());
+        }
+
+        [Fact]
+        public async Task GetBitcoinSV_Test()
+        {
+            var api = new CoinGecko();
+            var bsv = (await api.GetSupportedCoins()).First(x => x.Symbol == "bsv");
+            Assert.Equal("bitcoin-cash-sv", bsv.Id);
+            Assert.Equal("bsv", bsv.Symbol);
+            Assert.Equal("Bitcoin SV", bsv.Name);
         }
     }
 }
