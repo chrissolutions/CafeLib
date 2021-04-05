cf4e67620c8bf499271460dd48dba8c7d4f3fcee
chrissolutions <chris@chrissolutions.com>
2021-04-05 15:04:01 +1200

    Added CoinGecko API to obtain SupportedCoin.

---
+++ Enterprise/Bitcoin/CafeLib.Bitcoin/CafeLib.Bitcoin.Api.CoinGecko/Models/SupportedCoin.cs
@@ -0,0 +1,9 @@
+﻿namespace CafeLib.Bitcoin.Api.CoinGecko.Models
+{
+    public class SupportedCoin
+    {
+        public string Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Enterprise/Bitcoin/CafeLib.Bitcoin/CafeLib.Bitcoin.UnitTests/APIs/CoinGeckoApiTests.cs
@@ -0,0 +1,21 @@
+﻿#region Copyright
+// Copyright (c) 2020 TonesNotes
+// Distributed under the Open BSV software license, see the accompanying file LICENSE.
+#endregion
+
+using System.Linq;
+using System.Threading.Tasks;
+using CafeLib.Bitcoin.Api.CoinGecko;
+using Xunit;
+
+namespace CafeLib.Bitcoin.UnitTests.APIs {
+    public class CoinGeckoApiTests {
+        [Fact]
+        public async Task GetSupportedCoinsTest() {
+            var api = new CoinGecko();
+            var coins = await api.GetSupportedCoins();
+            Assert.NotNull(coins);
+            Assert.True(coins.Any());
+        }
+    }
+}
