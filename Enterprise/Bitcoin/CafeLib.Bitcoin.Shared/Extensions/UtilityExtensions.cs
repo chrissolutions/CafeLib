d72a2abb9a0c5a216293737716a9b9f2bcc2c90b
chrissolutions <chris@chrissolutions.com>
2021-04-12 10:29:53 +1200

    Added HexToBytes to UtilityExtensions

--- Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Extensions/UtilityExtensions.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Extensions/UtilityExtensions.cs
@@ -1,7 +1,11 @@
-﻿namespace CafeLib.Bitcoin.Shared.Extensions
+﻿using CafeLib.Bitcoin.Shared.Encoding;
+
+namespace CafeLib.Bitcoin.Shared.Extensions
 {
     public static class UtilityExtensions
     {
         public static byte[] Utf8ToBytes(this string s) => System.Text.Encoding.UTF8.GetBytes(s);
+        public static byte[] HexToBytes(this string s) => Encoders.Hex.Decode(s);
+
     }
 }
