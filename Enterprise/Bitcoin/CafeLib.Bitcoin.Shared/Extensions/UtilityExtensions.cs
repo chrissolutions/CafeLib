75e889902f3d083cca67f0318e3349ce207806ca
chrissolutions <chris@chrissolutions.com>
2021-04-12 09:29:58 +1200

    Renamed BitcoinExtensions to UtilityExtensions

--- Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Extensions/UtilityExtensions.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Extensions/UtilityExtensions.cs
@@ -1,6 +1,6 @@
 ﻿namespace CafeLib.Bitcoin.Shared.Extensions
 {
-    public static class BitcoinExtensions
+    public static class UtilityExtensions
     {
         public static byte[] Utf8ToBytes(this string s) => System.Text.Encoding.UTF8.GetBytes(s);
     }
