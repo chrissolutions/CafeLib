a4b7c0187ff388583660c7e6d234646543fb2f46
chrissolutions <chris@chrissolutions.com>
2021-04-13 11:17:24 +1200

    Migration of Base58ExtPrivateKey

--- Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Extensions/UtilityExtensions.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Extensions/UtilityExtensions.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using CafeLib.Bitcoin.Shared.Buffers;
 using CafeLib.Bitcoin.Shared.Encoding;
 
 namespace CafeLib.Bitcoin.Shared.Extensions
@@ -7,5 +9,8 @@ namespace CafeLib.Bitcoin.Shared.Extensions
     public static class UtilityExtensions
     {
         public static int GetHashCodeOfValues(this IEnumerable<byte> bytes) => bytes?.Aggregate(17, (current, b) => current * 31 + b) ?? 0;
+
+        public static Span<byte> Slice(this byte[] a, int start) => a.AsSpan().Slice(start);
+        public static ByteSpan Slice(this byte[] a, int start, int length) => a.AsSpan().Slice(start, length);
     }
 }
