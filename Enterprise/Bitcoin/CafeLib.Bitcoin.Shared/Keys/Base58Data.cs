a35172a25e29f63a8a8f709285a7683dd78cee7b
chrissolutions <chris@chrissolutions.com>
2021-04-12 21:54:04 +1200

    Updates to Base58Data

--- Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Keys/Base58Data.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Keys/Base58Data.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Linq;
+using CafeLib.Bitcoin.Shared.Buffers;
 using CafeLib.Bitcoin.Shared.Encoding;
 
 namespace CafeLib.Bitcoin.Shared.Keys
@@ -14,14 +15,14 @@ namespace CafeLib.Bitcoin.Shared.Keys
     /// </summary>
     public class Base58Data : IComparable<Base58Data>
     {
-        protected byte[] _VersionData;
-        protected int _VersionLength;
+        private byte[] _versionData;
+        private int _versionLength;
 
-        protected Span<byte> _Version => new Span<byte>(_VersionData, 0, _VersionLength);
-        protected Span<byte> _Data => new Span<byte>(_VersionData, _VersionLength, _VersionData.Length - _VersionLength);
-        protected ReadOnlySpan<byte> VersionData => _VersionData.AsSpan();
-        protected ReadOnlySpan<byte> Version => _Version;
-        protected ReadOnlySpan<byte> Data => _Data;
+        protected ByteSpan Version => new Span<byte>(_versionData, 0, _versionLength);
+        protected ByteSpan Data => new Span<byte>(_versionData, _versionLength, _versionData.Length - _versionLength);
+        protected ReadOnlyByteSpan VersionData => _versionData.AsSpan();
+        //protected ReadOnlySpan<byte> Version => _Version;
+        //protected ReadOnlySpan<byte> Data => _Data;
 
         protected Base58Data()
         {
@@ -29,50 +30,50 @@ namespace CafeLib.Bitcoin.Shared.Keys
 
         protected void SetData(byte[] versionData, int versionLength = 1)
         {
-            _VersionData = versionData;
-            _VersionLength = versionLength;
+            _versionData = versionData;
+            _versionLength = versionLength;
         }
 
         protected void SetData(ReadOnlySpan<byte> version, ReadOnlySpan<byte> data, bool flag)
         {
-            _VersionData = new byte[version.Length + data.Length + 1];
-            _VersionLength = version.Length;
-            version.CopyTo(_Version);
-            data.CopyTo(_Data);
-            _Data[^1] = (byte)(flag ? 1 : 0);
+            _versionData = new byte[version.Length + data.Length + 1];
+            _versionLength = version.Length;
+            version.CopyTo(Version);
+            data.CopyTo(Data);
+            Data.Data[^1] = (byte)(flag ? 1 : 0);
         }
 
         protected void SetData(ReadOnlySpan<byte> version, ReadOnlySpan<byte> data)
         {
-            _VersionData = new byte[version.Length + data.Length];
-            _VersionLength = version.Length;
-            version.CopyTo(_Version);
-            data.CopyTo(_Data);
+            _versionData = new byte[version.Length + data.Length];
+            _versionLength = version.Length;
+            version.CopyTo(Version);
+            data.CopyTo(Data);
         }
 
         protected bool SetString(string b58, int nVersionBytes)
         {
             if (!Encoders.Base58Check.TryDecode(b58, out var bytes) || bytes.Length < nVersionBytes)
             {
-                _VersionData = new byte[0];
-                _VersionLength = 0;
+                _versionData = new byte[0];
+                _versionLength = 0;
                 return false;
             }
 
-            _VersionData = bytes;
-            _VersionLength = nVersionBytes;
+            _versionData = bytes;
+            _versionLength = nVersionBytes;
             return true;
         }
 
-        public override string ToString() => Encoders .Base58Check.Encode(_VersionData);
+        public override string ToString() => Encoders .Base58Check.Encode(_versionData);
 
         public override int GetHashCode() => ToString().GetHashCode();
-        public bool Equals(Base58Data o) => (object)o != null && Enumerable.SequenceEqual(_VersionData, o._VersionData);
-        public override bool Equals(object obj) => obj is Base58Data && this == (Base58Data)obj;
-        public static bool operator ==(Base58Data x, Base58Data y) => object.ReferenceEquals(x, y) || (object)x == null && (object)y == null || x.Equals(y);
+        public bool Equals(Base58Data o) => (object)o != null && Enumerable.SequenceEqual(_versionData, o._versionData);
+        public override bool Equals(object obj) => obj is Base58Data base58Data && this == base58Data;
+        public static bool operator ==(Base58Data x, Base58Data y) => ReferenceEquals(x, y) || (object)x == null && (object)y == null || x.Equals(y);
         public static bool operator !=(Base58Data x, Base58Data y) => !(x == y);
 
-        public int CompareTo(Base58Data o) => o == null ? 1 : VersionData.SequenceCompareTo(o.VersionData);
+        public int CompareTo(Base58Data o) => o == null ? 1 : VersionData.Data.SequenceCompareTo(o.VersionData);
         public static bool operator <(Base58Data a, Base58Data b) => a.CompareTo(b) < 0;
         public static bool operator >(Base58Data a, Base58Data b) => a.CompareTo(b) > 0;
         public static bool operator <=(Base58Data a, Base58Data b) => a.CompareTo(b) <= 0;
