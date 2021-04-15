a2a76238c41f4b88583c38d0ed49f098761eed3b
chrissolutions <chris@chrissolutions.com>
2021-04-15 18:44:29 +1200

    Migrated WriterLength

---
+++ Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Persistence/WriterLength.cs
@@ -0,0 +1,74 @@
+﻿#region Copyright
+// Copyright (c) 2020 TonesNotes
+// Distributed under the Open BSV software license, see the accompanying file LICENSE.
+#endregion
+
+using CafeLib.Bitcoin.Shared.Buffers;
+using CafeLib.Bitcoin.Shared.Numerics;
+
+namespace CafeLib.Bitcoin.Shared.Persistence
+{
+    public class WriterLength : IBitcoinWriter
+    {
+        public long Length;
+
+        public IBitcoinWriter Add(ReadOnlyByteSpan data)
+        {
+            Length += data.Length; 
+            return this;
+        }
+
+        public IBitcoinWriter Add(ReadOnlyByteSequence data)
+        {
+            Length += data.Length; 
+            return this;
+        }
+
+        public IBitcoinWriter Add(long v)
+        {
+            Length += sizeof(long);
+            return this;
+        }
+
+        public IBitcoinWriter Add(ulong v)
+        {
+            Length += sizeof(ulong); 
+            return this;
+        }
+        public IBitcoinWriter Add(int v)
+        {
+            Length += sizeof(int);
+            return this;
+        }
+
+        public IBitcoinWriter Add(uint v)
+        {
+            Length += sizeof(uint); 
+            return this;
+        }
+        
+        public IBitcoinWriter Add(byte v)
+        {
+            Length += sizeof(byte); 
+            return this;
+        }
+
+        public IBitcoinWriter Add(UInt160 v)
+        {
+            Length += UInt160.Length; 
+            return this;
+        }
+
+        public IBitcoinWriter Add(UInt256 v)
+        {
+            Length += UInt256.Length; 
+            return this;
+        }
+
+        public IBitcoinWriter Add(UInt512 v)
+        {
+            Length += UInt256.Length; 
+            return this;
+        } 
+    }
+}
