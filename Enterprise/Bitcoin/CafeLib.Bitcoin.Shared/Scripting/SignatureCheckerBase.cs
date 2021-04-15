e88162027fa32b17076b0a0083335464c130b348
chrissolutions <chris@chrissolutions.com>
2021-04-16 09:39:50 +1200

    Migrating SignatureCheckerBase

---
+++ Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Scripting/SignatureCheckerBase.cs
@@ -0,0 +1,15 @@
+﻿#region Copyright
+// Copyright (c) 2020 TonesNotes
+// Distributed under the Open BSV software license, see the accompanying file LICENSE.
+#endregion
+namespace CafeLib.Bitcoin.Shared.Scripting
+{
+    public class SignatureCheckerBase
+    {
+        //public virtual bool CheckSig(ValType scriptSig, ValType vchPubKey, Script script, ScriptFlags flags) => false;
+
+        public virtual bool CheckLockTime(ScriptNum nLockTime) => false;
+
+        public virtual bool CheckSequence(ScriptNum nSequence) => false;
+    }
+}
