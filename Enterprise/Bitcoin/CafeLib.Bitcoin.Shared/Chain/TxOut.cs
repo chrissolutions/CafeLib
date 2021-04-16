0b0cd3bb7a2ada4a00595fbec2ae73a68dd7672c
chrissolutions <chris@chrissolutions.com>
2021-04-16 16:47:43 +1200

    Migrated TxOut

--- Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Chain/TxOut.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Chain/TxOut.cs
@@ -3,9 +3,9 @@
 // Distributed under the Open BSV software license, see the accompanying file LICENSE.
 #endregion
 
-using System;
 using System.IO;
 using CafeLib.Bitcoin.Shared.Buffers;
+using CafeLib.Bitcoin.Shared.Extensions;
 using CafeLib.Bitcoin.Shared.Persistence;
 using CafeLib.Bitcoin.Shared.Scripting;
 using CafeLib.Bitcoin.Shared.Units;
@@ -14,15 +14,19 @@ namespace CafeLib.Bitcoin.Shared.Chain
 {
     public struct TxOut
     {
-        Int64 _value;
-        Script _scriptPub;
+        private long _value;
+        private Script _script;
 
-        public Int64 Value => _value;
-        public Script ScriptPub => _scriptPub;
+        public long Value => _value;
+        public Script Script => _script;
 
-        public static TxOut Null => new TxOut() { _value = -1 };
+        public static TxOut Null => new TxOut { _value = -1 };
 
-        public TxOut(Int64 value, Script scriptPub) { _value = value; _scriptPub = scriptPub; }
+        public TxOut(long value, Script script)
+        {
+            _value = value;
+            _script = script;
+        }
 
         public bool IsNull => _value == -1;
 
@@ -32,7 +36,7 @@ namespace CafeLib.Bitcoin.Shared.Chain
 
             bp.TxOutStart(this, r.Data.Consumed);
 
-            if (!_scriptPub.TryParseScript(ref r, bp)) goto fail;
+            if (!_script.TryParseScript(ref r, bp)) goto fail;
 
             bp.TxOutParsed(this, r.Data.Consumed);
 
@@ -44,7 +48,7 @@ namespace CafeLib.Bitcoin.Shared.Chain
         public bool TryReadTxOut(ref ByteSequenceReader reader)
         {
             if (!reader.TryReadLittleEndian(out _value)) goto fail;
-            if (!_scriptPub.TryReadScript(ref reader)) goto fail;
+            if (!_script.TryReadScript(ref reader)) goto fail;
 
             return true;
         fail:
@@ -54,25 +58,25 @@ namespace CafeLib.Bitcoin.Shared.Chain
         public void Write(BinaryWriter s)
         {
             s.Write(_value);
-            _scriptPub.Write(s);
+            _script.Write(s);
         }
 
         public void Read(BinaryReader s)
         {
             _value = s.ReadInt64();
-            _scriptPub.Read(s);
+            _script.Read(s);
         }
 
         public override string ToString()
         {
-            return $"{new Amount(_value)} {_scriptPub}";
+            return $"{new Amount(_value)} {_script}";
         }
 
         public IBitcoinWriter AddTo(IBitcoinWriter writer)
         {
             writer
                 .Add(_value)
- //               .Add(_scriptPub)
+                .Add(_script)
                 ;
             return writer;
         }
--- Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Extensions/WriterExtensions.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin.Shared/Extensions/WriterExtensions.cs
@@ -9,7 +9,9 @@ namespace CafeLib.Bitcoin.Shared.Extensions
 {
     public static class WriterExtensions
     {
-        public static IBitcoinWriter Add(this IBitcoinWriter w, Script script, bool withoutCodeSeparators = false) => script.AddTo(w, withoutCodeSeparators);
+        public static IBitcoinWriter Add(this IBitcoinWriter w, Script script, bool withoutCodeSeparators = false)
+            => script.AddTo(w, withoutCodeSeparators);
+
         public static IBitcoinWriter Add(this IBitcoinWriter w, OutPoint op) => op.AddTo(w);
         //public static IBitcoinWriter Add(this IBitcoinWriter w, KzTxIn txIn) => txIn.AddTo(w);
         //public static IBitcoinWriter Add(this IBitcoinWriter w, KzTxOut txOut) => txOut.AddTo(w);
