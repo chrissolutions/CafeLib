0c62050c4c9af8c65d400211995b7d81202d92ed
chrissolutions <chris@chrissolutions.com>
2021-04-19 07:33:21 +1200

    Stubbed RpcTests

--- Enterprise/Bitcoin/CafeLib.Bitcoin.UnitTests/Network/RpcTests.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin.UnitTests/Network/RpcTests.cs
@@ -4,20 +4,20 @@ using CafeLib.Bitcoin.Network;
 namespace CafeLib.Bitcoin.UnitTests.Network {
     public class RpcTests {
 
-        public KzRpcClient GetKzRpc() {
-            // Use command setx KZRPCAUTH "{username}:{password}"  to set this environment variable. Then restart Visual Studio to access.
-            var auth = Environment.GetEnvironmentVariable("KZRPCAUTH");
-            var addr = Environment.GetEnvironmentVariable("KZRPCADDR");
-            var uri = new Uri(addr);
+        //public KzRpcClient GetKzRpc() {
+        //    // Use command setx KZRPCAUTH "{username}:{password}"  to set this environment variable. Then restart Visual Studio to access.
+        //    var auth = Environment.GetEnvironmentVariable("KZRPCAUTH");
+        //    var addr = Environment.GetEnvironmentVariable("KZRPCADDR");
+        //    var uri = new Uri(addr);
 
-            var kzrpc = new KzRpcClient(auth, uri);
-            return kzrpc;
-        }
+        //    var kzrpc = new KzRpcClient(auth, uri);
+        //    return kzrpc;
+        //}
 
-        //[Fact]
-        public void GetPeerInfoTest() {
-            var rpc = GetKzRpc();
-            var pi = rpc.GetPeerInfo();
-        }
+        ////[Fact]
+        //public void GetPeerInfoTest() {
+        //    var rpc = GetKzRpc();
+        //    var pi = rpc.GetPeerInfo();
+        //}
     }
 }
