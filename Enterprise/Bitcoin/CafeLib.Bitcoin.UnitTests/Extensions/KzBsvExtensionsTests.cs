edfc84af69fb6b3e056f7df3a4d92cf92ed7213e
chrissolutions <chris@chrissolutions.com>
2021-04-19 16:41:29 +1200

    Moved ToInt32 testcase to Encode Endian testcase.

--- Enterprise/Bitcoin/CafeLib.Bitcoin.UnitTests/Extensions/KzBsvExtensionsTests.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin.UnitTests/Extensions/KzBsvExtensionsTests.cs
@@ -11,11 +11,12 @@ using Xunit;
 namespace CafeLib.Bitcoin.UnitTests.Extensions {
     public class KzBsvExtensionsTests {
         [Fact]
-        public void ToInt32() {
+        public void ToInt32()
+        {
             var bytes = new ByteSpan(new byte[] { 0xAC, 0x1E, 0xED, 0x88 });
 
             var le = Encoders.Endian.LittleEndianInt32(bytes);
-            var be = Encoders.Endian.LittleEndianInt32(bytes);
+            var be = Encoders.Endian.BigEndianInt32(bytes);
 
             Assert.Equal(unchecked((Int32)(0x88ED1EAC)), le);
             Assert.Equal(unchecked((Int32)(0xAC1EED88)), be);
