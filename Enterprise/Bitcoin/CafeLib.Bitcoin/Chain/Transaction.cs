8f25df0867acbf3ff79f6255c3f29ad8fcf02cd2
chrissolutions <chris@chrissolutions.com>
2021-04-19 14:25:16 +1200

    Fixed test exception in Transaction

--- Enterprise/Bitcoin/CafeLib.Bitcoin/Chain/Transaction.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin/Chain/Transaction.cs
@@ -5,8 +5,10 @@
 
 using System;
 using System.Buffers;
+using System.Linq;
 using System.Security.Cryptography;
 using CafeLib.Bitcoin.Buffers;
+using CafeLib.Bitcoin.Builders;
 using CafeLib.Bitcoin.Extensions;
 using CafeLib.Bitcoin.Numerics;
 using CafeLib.Bitcoin.Persistence;
@@ -20,7 +22,7 @@ namespace CafeLib.Bitcoin.Chain
     /// In particular, script data is stored as <see cref="ReadOnlySequence{Byte}"/> allowing large scripts to
     /// remain in whatever buffers were originally used. No script parsing data is maintained. 
     /// Not intended for making dynamic changes to a transaction (adding inputs, outputs, signing).
-    /// See <see cref="KzBTransaction"/> when dynamically building a transaction to send.
+    /// See <see cref="TransactionBuilder"/> when dynamically building a transaction to send.
     /// In addition, transactions associated with specific wallets should consider KzWallets.KzWdbTx for transaction data.
     /// Includes the following pre-computed meta data in addition to standard Bitcoin transaction data:
     /// <list type="table">
@@ -29,46 +31,56 @@ namespace CafeLib.Bitcoin.Chain
     /// </summary>
     public class Transaction
     {
+        /// Essential fields of a Bitcoin SV transaction.
+        private int _version;
+        private TxIn[] _vin = new TxIn[0];
+        private TxOut[] _vout = new TxOut[0];
+        private UInt32 _lockTime;
+
+        /// The following fields are computed or external, not essential.
+        private readonly UInt256 _hashTx = new UInt256();
+        //Int64 _valueIn;
+        //Int64 _valueOut;
+
         /// Public access to essential header fields.
+        public Int32 Version => _version;
+        public uint LockTime => _lockTime;
 
-        public int Version { get; set; }
-
-        public UInt32 LockTime { get; set; }
-
-        public TxIn[] Inputs { get; set; } = new TxIn[0];
-
-        public TxOut[] Outputs { get; set; } = new TxOut[0];
+        public TxIn[] Inputs => _vin;
+        public TxOut[] Outputs => _vout;
 
         /// Public access to computed or external, not essential.
 
-        public UInt256 Hash { get; }
+        public UInt256 Hash => _hashTx;
 
-        public Transaction()
+        public Transaction() { }
+
+        public Transaction(int version, TxIn[] vin, TxOut[] vout, uint lockTime)
         {
+            _version = version;
+            _vin = vin;
+            _vout = vout;
+            _lockTime = lockTime;
         }
 
-        public Transaction(Int32 version, TxIn[] vin, TxOut[] vout, UInt32 lockTime)
+        public Transaction(TransactionBuilder tb)
         {
-            Version = version;
-            Inputs = vin;
-            Outputs = vout;
-            LockTime = lockTime;
+            _version = tb.Version;
+            _vin = tb.Vin.Select(i => i.ToTxIn()).ToArray();
+            _vout = tb.Vout.Select(o => o.ToTxOut()).ToArray();
+            _lockTime = tb.LockTime;
         }
-
-        //public Transaction(KzBTransaction tb)
-        //{
-        //    _version = tb.Version;
-        //    _vin = tb.Vin.Select(i => i.ToTxIn()).ToArray();
-        //    _vout = tb.Vout.Select(o => o.ToTxOut()).ToArray();
-        //    _lockTime = tb.LockTime;
-        //}
 
         public bool TryReadTransaction(ref ReadOnlyByteSequence ros)
         {
             var r = new ByteSequenceReader(ros);
-            if (!TryReadTransaction(ref r)) return false;
+            if (!TryReadTransaction(ref r)) goto fail;
+
             ros = ros.Data.Slice(r.Data.Consumed);
+
             return true;
+            fail:
+            return false;
         }
 
         public bool TryParseTransaction(ref ByteSequenceReader r, IBlockParser bp)
@@ -76,28 +88,28 @@ namespace CafeLib.Bitcoin.Chain
             var offset = r.Data.Consumed;
             var start = r.Data.Position;
 
-            if (!r.TryReadLittleEndian(out int version)) goto fail;
-            if (!r.TryReadVarInt(out var countIn)) goto fail;
+            if (!r.TryReadLittleEndian(out _version)) return false;;
+            if (!r.TryReadVarInt(out var countIn)) return false;
 
             bp.TxStart(this, offset);
 
-            var vin = new TxIn[countIn];
+            _vin = new TxIn[countIn];
             for (var i = 0L; i < countIn; i++)
             {
-                ref var txIn = ref vin[i];
-                if (!txIn.TryParseTxIn(ref r, bp)) goto fail;
+                ref var txIn = ref _vin[i];
+                if (!txIn.TryParseTxIn(ref r, bp)) return false;
             }
 
-            if (!r.TryReadVarInt(out var countOut)) goto fail;
+            if (!r.TryReadVarInt(out var countOut)) return false;
 
-            var vout = new TxOut[countOut];
+            _vout = new TxOut[countOut];
             for (var i = 0L; i < countOut; i++)
             {
-                ref var txOut = ref vout[i];
-                if (!txOut.TryParseTxOut(ref r, bp)) goto fail;
+                ref var txOut = ref _vout[i];
+                if (!txOut.TryParseTxOut(ref r, bp)) return false;
             }
 
-            if (!r.TryReadLittleEndian(out uint lockTime)) goto fail;
+            if (!r.TryReadLittleEndian(out _lockTime)) return false;
 
             var end = r.Data.Position;
 
@@ -107,59 +119,54 @@ namespace CafeLib.Bitcoin.Chain
             {
                 var hash1 = sha256.ComputeHash(txBytes);
                 var hash2 = sha256.ComputeHash(hash1);
-                hash2.CopyTo(Hash.Span);
+                hash2.CopyTo(_hashTx.Span);
             }
 
             bp.TxParsed(this, r.Data.Consumed);
 
-            Version = version;
-            Inputs = vin;
-            Outputs = vout;
-            LockTime = lockTime;
             return true;
-
-            fail:
-            return false;
         }
 
         public bool TryReadTransaction(ref ByteSequenceReader r)
         {
             var start = r.Data.Position;
 
-            if (!r.TryReadLittleEndian(out int version)) return false;
-            if (!r.TryReadVarInt(out var countIn)) return false;
+            if (!r.TryReadLittleEndian(out _version)) goto fail;
+            if (!r.TryReadVarInt(out var countIn)) goto fail;
 
-            var vin = new TxIn[countIn];
+            _vin = new TxIn[countIn];
             for (var i = 0L; i < countIn; i++)
             {
-                ref var txIn = ref Inputs[i];
-                if (!txIn.TryReadTxIn(ref r)) return false;
+                ref var txIn = ref _vin[i];
+                if (!txIn.TryReadTxIn(ref r)) goto fail;
             }
 
-            if (!r.TryReadVarInt(out var countOut)) return false;
+            if (!r.TryReadVarInt(out long countOut)) goto fail;
 
-            var vout = new TxOut[countOut];
+            _vout = new TxOut[countOut];
             for (var i = 0L; i < countOut; i++)
             {
-                ref var txOut = ref Outputs[i];
-                if (!txOut.TryReadTxOut(ref r)) return false;
+                ref var txOut = ref _vout[i];
+                if (!txOut.TryReadTxOut(ref r)) goto fail;
             }
 
-            if (!r.TryReadLittleEndian(out uint lockTime)) return false;
+            if (!r.TryReadLittleEndian(out _lockTime)) goto fail;
 
             var end = r.Data.Position;
 
             // Compute the transaction hash.
             var txBytes = r.Data.Sequence.Slice(start, end).ToArray();
-            using var sha256 = SHA256.Create();
-            var hash1 = sha256.ComputeHash(txBytes);
-            var hash2 = sha256.ComputeHash(hash1);
-            hash2.CopyTo(Hash.Span);
-            Version = version;
-            Inputs = vin;
-            Outputs = vout;
-            LockTime = lockTime;
+            using (var sha256 = SHA256.Create())
+            {
+                var hash1 = sha256.ComputeHash(txBytes);
+                var hash2 = sha256.ComputeHash(hash1);
+                hash2.CopyTo(_hashTx.Span);
+            }
+
             return true;
+
+            fail:
+            return false;
         }
 
         public static Transaction ParseHex(string rawTxHex)
@@ -179,23 +186,22 @@ namespace CafeLib.Bitcoin.Chain
         public IBitcoinWriter AddTo(IBitcoinWriter writer)
         {
             writer
-                .Add(Version)
-                .Add(Inputs.Length.AsVarIntBytes())
+                .Add(_version)
+                .Add(_vin.Length.AsVarIntBytes())
                 ;
-
-            foreach (var txIn in Inputs)
-                //writer
-                //.Add(txIn)
-                ;
-            writer
-                .Add(Outputs.Length.AsVarIntBytes())
-                ;
-            foreach (var txOut in Outputs)
-                //writer
-                //    .Add(txOut)
+            foreach (var txIn in _vin)
+                writer
+                    .Add(txIn)
                     ;
             writer
-                .Add(LockTime)
+                .Add(_vout.Length.AsVarIntBytes())
+                ;
+            foreach (var txOut in _vout)
+                writer
+                    .Add(txOut)
+                    ;
+            writer
+                .Add(_lockTime)
                 ;
             return writer;
         }
