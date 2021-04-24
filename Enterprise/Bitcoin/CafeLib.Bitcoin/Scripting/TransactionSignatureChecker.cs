97f39bc4ced9b76985d27679944c2496436175dd
chrissolutions <chris@chrissolutions.com>
2021-04-24 13:53:41 +1200

    Preparing TransactionSignatureChecker

--- Enterprise/Bitcoin/CafeLib.Bitcoin/Scripting/TransactionSignatureChecker.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin/Scripting/TransactionSignatureChecker.cs
@@ -9,38 +9,48 @@ namespace CafeLib.Bitcoin.Scripting
 {
     public class TransactionSignatureChecker : SignatureCheckerBase
     {
-        public override bool CheckSignature(ValType scriptSig, ValType vchPubKey, Script script, ScriptFlags flags)
+        public TransactionSignatureChecker()
         {
-            const std::vector<uint8_t> &vchSigIn, const std::vector<uint8_t> &vchPubKey,
-            const CScript &scriptCode, bool enabledSighashForkid) const {
-                CPubKey pubkey(vchPubKey);
-                if (!pubkey.IsValid())
-                {
-                    return false;
-                }
 
-                // Hash type is one byte tacked on to the end of the signature
-                std::vector<uint8_t> vchSig(vchSigIn);
-                if (vchSig.empty())
-                {
-                    return false;
-                }
-                SigHashType sigHashType = GetHashType(vchSig);
-                vchSig.pop_back();
+        }
 
-                uint256 sighash = SignatureHash(scriptCode, *txTo, nIn, sigHashType, amount,
-                    this->txdata, enabledSighashForkid);
 
-                if (!VerifySignature(vchSig, pubkey, sighash))
-                {
-                    return false;
-                }
+        public override bool CheckSignature(VarType scriptSig, VarType vchPubKey, Script script, ScriptFlags flags)
+        {
+            return false;
 
-                return true;
-            }
+        //    const std::vector<uint8_t> &vchSigIn, const std::vector<uint8_t> &vchPubKey,
+        //    const CScript &scriptCode, bool enabledSighashForkid) const
+        //    {
+        //        CPubKey pubkey(vchPubKey);
+        //        if (!pubkey.IsValid())
+        //        {
+        //            return false;
+        //        }
 
-        public virtual bool CheckLockTime(ScriptNum nLockTime) => false;
+        //        // Hash type is one byte tacked on to the end of the signature
+        //        std::vector<uint8_t> vchSig(vchSigIn);
+        //        if (vchSig.empty())
+        //        {
+        //            return false;
+        //        }
+        //        SigHashType sigHashType = GetHashType(vchSig);
+        //        vchSig.pop_back();
 
-        public virtual bool CheckSequence(ScriptNum nSequence) => false;
+        //        uint256 sighash = SignatureHash(scriptCode, *txTo, nIn, sigHashType, amount,
+        //            this->txdata, enabledSighashForkid);
+
+        //        if (!VerifySignature(vchSig, pubkey, sighash))
+        //        {
+        //            return false;
+        //        }
+
+        //        return true;
+        //    }
+        }
+
+        public override bool CheckLockTime(ScriptNum nLockTime) => false;
+
+        public override bool CheckSequence(ScriptNum nSequence) => false;
     }
 }
