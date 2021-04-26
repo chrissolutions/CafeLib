da165f86cb8a5669d536c6815b7e36433d22370e
chrissolutions <chris@chrissolutions.com>
2021-04-27 08:48:44 +1200

    Restored TransactionSignatureChecker.cs

---
+++ Enterprise/Bitcoin/CafeLib.Bitcoin/Scripting/TransactionSignatureChecker.cs
@@ -0,0 +1,73 @@
+﻿#region Copyright
+// Copyright (c) 2020 TonesNotes
+// Distributed under the Open BSV software license, see the accompanying file LICENSE.
+#endregion
+
+using CafeLib.Bitcoin.Chain;
+using CafeLib.Bitcoin.Keys;
+using CafeLib.Bitcoin.Numerics;
+using CafeLib.Bitcoin.Persistence;
+using CafeLib.Bitcoin.Units;
+
+namespace CafeLib.Bitcoin.Scripting
+{
+    public class TransactionSignatureChecker : SignatureCheckerBase
+    {
+        private Transaction _tx;
+        private int _index;
+        private Amount _amount;
+
+        public TransactionSignatureChecker(Transaction tx, int index, Amount amount)
+        {
+            _tx = tx;
+            _index = index;
+            _amount = amount;
+
+            var prevOuts = new OutPoint(tx.Hash, index);
+
+        }
+
+        public override bool CheckSignature(VarType scriptSig, VarType vchPubKey, Script script, ScriptFlags flags)
+        {
+            var publicKey = new PublicKey(vchPubKey);
+            if (!publicKey.IsValid) return false;
+            if (vchPubKey == VarType.Empty) return false;
+            var sigHashType = new SignatureHashType(scriptSig.LastByte);
+
+            return false;
+
+            //    const std::vector<uint8_t> &vchSigIn, const std::vector<uint8_t> &vchPubKey,
+            //    const CScript &scriptCode, bool enabledSighashForkid) const
+            //    {
+            //        CPubKey pubkey(vchPubKey);
+            //        if (!pubkey.IsValid())
+            //        {
+            //            return false;
+            //        }
+
+            //        // Hash type is one byte tacked on to the end of the signature
+            //        std::vector<uint8_t> vchSig(vchSigIn);
+            //        if (vchSig.empty())
+            //        {
+            //            return false;
+            //        }
+            //        SigHashType sigHashType = GetHashType(vchSig);
+            //        vchSig.pop_back();
+
+            //        uint256 sighash = SignatureHash(scriptCode, *txTo, nIn, sigHashType, amount,
+            //            this->txdata, enabledSighashForkid);
+
+            //        if (!VerifySignature(vchSig, pubkey, sighash))
+            //        {
+            //            return false;
+            //        }
+
+            //        return true;
+            //    }
+        }
+
+        public override bool CheckLockTime(ScriptNum nLockTime) => false;
+
+        public override bool CheckSequence(ScriptNum nSequence) => false;
+    }
+}
