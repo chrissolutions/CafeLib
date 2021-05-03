c9a17fcf4180b13a0493458a9f79c311f46532b9
chrissolutions <chris@chrissolutions.com>
2021-05-04 11:26:22 +1200

    Fixed Retry.cs to property handle the retry operation definitions.

--- Core/CafeLib.Core/Support/Retry.cs
+++ Core/CafeLib.Core/Support/Retry.cs
@@ -10,6 +10,7 @@ namespace CafeLib.Core.Support
 
         private const int DefaultLimit = 3;
         private const int DefaultInterval = 50; // milliseconds
+        private const int DefaultIntervalGrowth = 0; // milliseconds
 
         #endregion
 
@@ -35,17 +36,43 @@ namespace CafeLib.Core.Support
         /// <summary>
         /// Run retry function.
         /// </summary>
+        /// <param name="limit"></param>
+        /// <param name="interval"></param>
+        /// <param name="operation"></param>
+        /// <returns>asynchronous task</returns>
+        public static Task Run(int limit, int interval, Action<int> operation)
+            => Run(limit, interval, DefaultIntervalGrowth, operation);
+
+        /// <summary>
+        /// Run retry function.
+        /// </summary>
         /// <param name="limit">retry limit</param>
         /// <param name="interval">interval between retries</param>
-        /// <param name="operation">retry function</param>
-        /// <returns>asynchronous task</returns>
-        public static async Task Run(int limit, int interval, Action<int> operation)
+        /// <param name="intervalGrowth">amount to grow interval in milliseconds between retries</param>
+        /// <param name="operation">retry operation</param>
+        public static async Task Run(int limit, int interval, int intervalGrowth, Action<int> operation)
         {
-            await Run(limit, interval, async x =>
+            var exceptions = new List<Exception>();
+
+            for (var retry = 0; retry < limit; retry++)
             {
-                operation(x);
-                await Task.CompletedTask;
-            });
+                try
+                {
+                    operation(retry + 1);
+                    return;
+                }
+                catch (TaskCanceledException)
+                {
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                    await Task.Delay(interval + retry * intervalGrowth);
+                }
+            }
+
+            throw new AggregateException(exceptions);
         }
 
         /// <summary>
@@ -76,9 +103,58 @@ namespace CafeLib.Core.Support
         /// <param name="operation">retry function</param>
         /// <returns>asynchronous task</returns>
         public static async Task<T> Run<T>(int limit, int interval, Func<int, T> operation)
+            => await Run(limit, interval, DefaultIntervalGrowth, operation);
+
+        /// <summary>
+        /// Run retry function.
+        /// </summary>
+        /// <typeparam name="T">return type</typeparam>
+        /// <param name="limit">retry limit</param>
+        /// <param name="interval">interval between retries</param>
+        /// <param name="intervalGrowth">amount to grow interval in milliseconds between retries</param>
+        /// <param name="operation">retry operation</param>
+        /// <returns>asynchronous task</returns>
+        public static async Task<T> Run<T>(int limit, int interval, int intervalGrowth, Func<int, T> operation)
         {
-            return await Run(limit, interval, x => Task.FromResult(operation(x)));
+            var exceptions = new List<Exception>();
+
+            for (var retry = 0; retry < limit; retry++)
+            {
+                try
+                {
+                    var result = operation(retry + 1);
+                    return result;
+                }
+                catch (TaskCanceledException)
+                {
+                    return default;
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                    await Task.Delay(interval + retry * intervalGrowth);
+                }
+            }
+
+            throw new AggregateException(exceptions);
         }
+
+        /// <summary>
+        /// Run retry operation.
+        /// </summary>
+        /// <param name="operation">retry operation</param>
+        /// <returns>asynchronous task</returns>
+        public static Task Run(Func<int, Task> operation)
+            => Run(DefaultLimit, operation);
+
+        /// <summary>
+        /// Run retry operation.
+        /// </summary>
+        /// <param name="limit">retry limit</param>
+        /// <param name="operation">retry operation</param>
+        /// <returns>asynchronous task</returns>
+        public static Task Run(int limit, Func<int, Task> operation)
+            => Run(limit, DefaultIntervalGrowth, operation);
 
         /// <summary>
         /// Run retry function.
@@ -86,8 +162,19 @@ namespace CafeLib.Core.Support
         /// <param name="limit"></param>
         /// <param name="interval"></param>
         /// <param name="operation"></param>
-        /// <returns></returns>
-        public static async Task Run(int limit, int interval, Func<int, Task> operation)
+        /// <returns>asynchronous task</returns>
+        public static Task Run(int limit, int interval, Func<int, Task> operation)
+            => Run(limit, interval, DefaultIntervalGrowth, operation);
+
+        /// <summary>
+        /// Run retry function.
+        /// </summary>
+        /// <param name="limit">retry limit</param>
+        /// <param name="interval">interval between retries</param>
+        /// <param name="intervalGrowth">amount to grow interval in milliseconds between retries</param>
+        /// <param name="operation">retry operation</param>
+        /// <returns>asynchronous task</returns>
+        public static async Task Run(int limit, int interval, int intervalGrowth, Func<int, Task> operation)
         {
             var exceptions = new List<Exception>();
 
@@ -105,12 +192,31 @@ namespace CafeLib.Core.Support
                 catch (Exception ex)
                 {
                     exceptions.Add(ex);
-                    await Task.Delay(interval);
+                    await Task.Delay(interval + retry * intervalGrowth);
                 }
             }
 
             throw new AggregateException(exceptions);
         }
+
+        /// <summary>
+        /// Run retry operation.
+        /// </summary>
+        /// <typeparam name="T">return type</typeparam>
+        /// <param name="operation">retry operation</param>
+        /// <returns>asynchronous task</returns>
+        public static Task<T> Run<T>(Func<int, Task<T>> operation)
+            => Run(DefaultLimit, operation);
+
+        /// <summary>
+        /// Run retry operation.
+        /// </summary>
+        /// <typeparam name="T">return type</typeparam>
+        /// <param name="limit">retry limit</param>
+        /// <param name="operation">retry operation</param>
+        /// <returns>asynchronous task</returns>
+        public static Task<T> Run<T>(int limit, Func<int, Task<T>> operation)
+            => Run(limit, DefaultIntervalGrowth, operation);
 
         /// <summary>
         /// Run retry function.
@@ -119,8 +225,20 @@ namespace CafeLib.Core.Support
         /// <param name="limit"></param>
         /// <param name="interval"></param>
         /// <param name="operation"></param>
-        /// <returns></returns>
-        public static async Task<T> Run<T>(int limit, int interval, Func<int, Task<T>> operation)
+        /// <returns>asynchronous task</returns>
+        public static Task<T> Run<T>(int limit, int interval, Func<int, Task<T>> operation)
+            => Run(limit, interval, DefaultIntervalGrowth, operation);
+
+        /// <summary>
+        /// Run retry function.
+        /// </summary>
+        /// <typeparam name="T">return type</typeparam>
+        /// <param name="limit"></param>
+        /// <param name="interval"></param>
+        /// <param name="intervalGrowth">amount to grow interval in milliseconds between retries</param>
+        /// <param name="operation"></param>
+        /// <returns>asynchronous task</returns>
+        public static async Task<T> Run<T>(int limit, int interval, int intervalGrowth, Func<int, Task<T>> operation)
         {
             var exceptions = new List<Exception>();
 
@@ -137,7 +255,7 @@ namespace CafeLib.Core.Support
                 catch (Exception ex)
                 {
                     exceptions.Add(ex);
-                    await Task.Delay(interval);
+                    await Task.Delay(interval + retry * intervalGrowth);
                 }
             }
 
