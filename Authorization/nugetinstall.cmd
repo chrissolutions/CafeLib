27e12b49b6d820979e1c908566628950de99a7aa
chrissolutions <chris@chrissolutions.com>
2021-05-20 17:34:52 +1200

    CafeLib.Authorization & CafeLib.Network nugetinstall to use dotnet command to publish nuget packages.

--- Authorization/nugetinstall.cmd
+++ Authorization/nugetinstall.cmd
@@ -5,8 +5,8 @@ setlocal
 set type=Authorization
 
 :: Settings
-set msbld=msbuild.exe
-set nuget=nuget.exe
+set msbld=dotnet build
+set nuget=dotnet nuget
 set configuration=Debug
 set libPath=bin\%configuration%
 set apikey=
@@ -38,7 +38,7 @@ echo Create Nuget Package for %solution% ...
 for %%X in (%libs%) DO @echo on&&%msbld% %sourcepath%\%%X\%%X.csproj -t:pack -p:PackageVersion=%version% -p:Configuration=%configuration%&&@echo off
 
 echo Push Package to Nuget repository ...
-for %%X in (%libs%) DO @echo on&&%nuget% push %sourcepath%\%%X\%libPath%\%%X.%version%.nupkg %apikey% -source %nugetRepo%&&@echo off
+for %%X in (%libs%) DO @echo on&&%nuget% push %sourcepath%\%%X\%libPath%\%%X.%version%.nupkg %apikey% -s %nugetRepo%&&@echo off
 goto exit
 
 :usage
