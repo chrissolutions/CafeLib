381983441d21de9379877513479e470af6fab47b
chrissolutions <chris@chrissolutions.com>
2021-05-24 10:26:56 +1200

    Added support for Wallet WordLists class to read words from embedded resources.

---
+++ Enterprise/Bitcoin/CafeLib.Bitcoin/Wallet/Languages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CafeLib.Bitcoin.Wallet
+{
+    public enum Languages
+    {
+        Unknown,
+        English,
+        Spanish,
+        French,
+        Italian,
+        Japanese,
+        PortugueseBrazil,
+        ChineseSimplified,
+        ChineseTraditional,
+    }
+}
---
+++ Enterprise/Bitcoin/CafeLib.Bitcoin/Wallet/WordLists.cs
@@ -0,0 +1,32 @@
+﻿#region Copyright
+// Copyright (c) 2020 TonesNotes
+// Distributed under the Open BSV software license, see the accompanying file LICENSE.
+#endregion
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CafeLib.Bitcoin.Extensions;
+using CafeLib.Bitcoin.Scripting;
+
+namespace CafeLib.Bitcoin.Wallet
+{
+    public static class WordLists
+    {
+        private static readonly IDictionary<Languages, string[]> CultureMap = new Dictionary<Languages, string[]>();
+
+        public static string[] GetWords(Languages language)
+        {
+            if (CultureMap.TryGetValue(language, out var words)) return words;
+            var manifest = $"{typeof(WordLists).Namespace}.Cultures.{language.GetName()}.words";
+            using var stream = typeof(WordLists).Assembly.GetManifestResourceStream(manifest);
+            using var reader = new StreamReader(stream ?? throw new NotSupportedException(nameof(language)));
+            var text = reader.ReadToEnd();
+            words = text.Split("\n");
+            CultureMap.Add(language, words);
+            return words;
+        }
+    }
+}
