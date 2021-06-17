0dc868bb35658abcb992283878f02c59fb713da6
chrissolutions <chris@chrissolutions.com>
2021-06-17 21:23:00 +1200

    Added additional Base58 tests.

--- Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp.UnitTests/Encode/KzBase58Tests.cs
+++ Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp.UnitTests/Encode/KzBase58Tests.cs
@@ -10,8 +10,42 @@ namespace CafeLib.BsvSharp.UnitTests.Encode
 {
     public class KzBase58Tests
     {
-        private static readonly HexEncoder Hex = Encoders.Hex;
-        private static readonly Base58Encoder Base58 = Encoders.Base58;
+        [Fact]
+        public void Base58EncodeDecodeViaInstance()
+        {
+            const string hex = "73696d706c792061206c6f6e6720737472696e67";
+            const string base58 = "2cFupjhnEsSn59qHXstmK2ffpLv2";
+
+            var bytes = Base58Encoder.Current.Decode(base58);
+            var text = Base58Encoder.Current.Encode(bytes);
+
+            Assert.Equal(base58, text);
+            Assert.Equal(hex, HexEncoder.Current.Encode(bytes));
+        }
+
+        [Fact]
+        public void Base58EncodeDecodeViaEncoders()
+        {
+            const string hex = "73696d706c792061206c6f6e6720737472696e67";
+            const string base58 = "2cFupjhnEsSn59qHXstmK2ffpLv2";
+
+            var bytes = Encoders.Base58.Decode(base58);
+            var text = Encoders.Base58.Encode(bytes);
+
+            Assert.Equal(base58, text);
+            Assert.Equal(hex, HexEncoder.EncodeBytes(bytes));
+        }
+
+        [Fact]
+        public void Base58VerifyHexEncoding()
+        {
+            const string hex = "73696d706c792061206c6f6e6720737472696e67";
+            const string base58 = "2cFupjhnEsSn59qHXstmK2ffpLv2";
+
+            Assert.Equal(base58, Encoders.Base58.FromHex(hex));
+            Assert.Equal(hex, Encoders.Base58.ToHex(base58));
+        }
+
 
         [Theory]
         [InlineData("", "")]
@@ -28,10 +62,10 @@ namespace CafeLib.BsvSharp.UnitTests.Encode
         [InlineData("00000000000000000000", "1111111111")]
         public void Base58TestCases(string hex, string base58)
         {
-            var hexValue = Hex.Decode(hex);
-            var bytes = Base58.Decode(base58);
+            var hexValue = Encoders.Hex.Decode(hex);
+            var bytes = Encoders.Base58.Decode(base58);
             Assert.Equal(hexValue, bytes);
-            Assert.Equal(base58, Base58.Encode(bytes));
+            Assert.Equal(base58, Encoders.Base58.Encode(bytes));
         }
     }
 }
