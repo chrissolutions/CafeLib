9ea1509db58ae5554ef55318cec43ef66ac7a890
chrissolutions <chris@chrissolutions.com>
2021-06-20 18:06:12 +1200

    Modified ByteArrayExtensions Concat to internally use span.

--- Core/CafeLib.Core/Extensions/ByteArrayExtensions.cs
+++ Core/CafeLib.Core/Extensions/ByteArrayExtensions.cs
@@ -23,17 +23,18 @@ namespace CafeLib.Core.Extensions
         /// </summary>
         public static byte[] Concat(this byte[] b, params byte[][] args)
         {
-            var arrays = new List<byte[]> {b};
-            arrays.AddRange(args);
-            var bytes = new byte[arrays.Sum(x => x.Length)];
+            var arrays = new byte[args.Length + 1][];
+            arrays[0] = b;
+            args.CopyTo(arrays.AsSpan()[1..]);
+            var bytes = new byte[arrays.Sum(x => x.Length)].AsSpan();
             var offset = 0;
             foreach (var array in arrays)
             {
-                Buffer.BlockCopy(array, 0, bytes, offset, array.Length);
+                array.CopyTo(bytes[offset..]);
                 offset += array .Length;
             }
 
-            return bytes;
+            return bytes.ToArray();
         }
     }
 }
