fc645cd56efe5f5321660b6e7399f0a74db539cf
chrissolutions <chris@chrissolutions.com>
2021-06-22 22:23:12 +1200

    EndianEncoder implements IEncoder

--- Enterprise/Bitcoin/CafeLib.Bitcoin/Encoding/EndianEncoder.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin/Encoding/EndianEncoder.cs
@@ -13,18 +13,12 @@ namespace CafeLib.Bitcoin.Encoding
         public long LittleEndianInt64(ReadOnlyByteSpan data) => BinaryPrimitives.ReadInt64LittleEndian(data);
         public long BigEndianInt64(ReadOnlyByteSpan data) => BinaryPrimitives.ReadInt64BigEndian(data);
 
-        public byte[] Decode(string source)
-        {
-            return BitConverter.IsLittleEndian
+        public byte[] Decode(string source) => BitConverter.IsLittleEndian
                 ? System.Text.Encoding.Unicode.GetBytes(source)
                 : System.Text.Encoding.BigEndianUnicode.GetBytes(source);
-       }
 
-        public string Encode(byte[] bytes)
-        {
-            return BitConverter.IsLittleEndian
+        public string Encode(byte[] bytes) => BitConverter.IsLittleEndian
                 ? System.Text.Encoding.Unicode.GetString(bytes) 
                 : System.Text.Encoding.BigEndianUnicode.GetString(bytes);
-        }
     }
 }
