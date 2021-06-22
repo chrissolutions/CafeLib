0203c4025b412a627bd7f0c891edf42f417a01c1
chrissolutions <chris@chrissolutions.com>
2021-06-22 22:12:59 +1200

    EndianEncoder implements IEncoder

--- Enterprise/Bitcoin/CafeLib.Bitcoin/Encoding/EndianEncoder.cs
+++ Enterprise/Bitcoin/CafeLib.Bitcoin/Encoding/EndianEncoder.cs
@@ -5,7 +5,7 @@ using CafeLib.Core.Support;
 
 namespace CafeLib.Bitcoin.Encoding
 {
-    public class EndianEncoder : SingletonBase<EndianEncoder>
+    public class EndianEncoder : SingletonBase<EndianEncoder>, IEncoder
     {
         public int LittleEndianInt32(ReadOnlyByteSpan data) => BinaryPrimitives.ReadInt32LittleEndian(data);
         public int BigEndianInt32(ReadOnlyByteSpan data) => BinaryPrimitives.ReadInt32BigEndian(data);
@@ -13,28 +13,18 @@ namespace CafeLib.Bitcoin.Encoding
         public long LittleEndianInt64(ReadOnlyByteSpan data) => BinaryPrimitives.ReadInt64LittleEndian(data);
         public long BigEndianInt64(ReadOnlyByteSpan data) => BinaryPrimitives.ReadInt64BigEndian(data);
 
+        public byte[] Decode(string source)
+        {
+            return BitConverter.IsLittleEndian
+                ? System.Text.Encoding.Unicode.GetBytes(source)
+                : System.Text.Encoding.BigEndianUnicode.GetBytes(source);
+       }
+
         public string Encode(byte[] bytes)
         {
             return BitConverter.IsLittleEndian
                 ? System.Text.Encoding.Unicode.GetString(bytes) 
                 : System.Text.Encoding.BigEndianUnicode.GetString(bytes);
-        }
-
-        public bool TryDecode(string encoded, out byte[] bytes)
-        {
-            bytes = default;
-            try
-            {
-                bytes = BitConverter.IsLittleEndian
-                    ? System.Text.Encoding.Unicode.GetBytes(encoded)
-                    : System.Text.Encoding.BigEndianUnicode.GetBytes(encoded);
-
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
         }
     }
 }
