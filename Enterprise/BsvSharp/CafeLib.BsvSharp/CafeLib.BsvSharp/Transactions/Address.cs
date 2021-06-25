84b369ff11dc85e5844e4f186f8101575a4efdf0
chrissolutions <chris@chrissolutions.com>
2021-06-26 08:15:59 +1200

    Implementing Address class

---
+++ Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp/Transactions/Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CafeLib.BsvSharp.Network;
+
+namespace CafeLib.BsvSharp.Transactions
+{
+    public class Address
+    {
+        private string _publicKeyHash;
+        //private AddressType _addressType;
+        private NetworkType _networkType;
+        private int _version;
+
+        /// Constructs a new Address object
+        ///
+        /// [address] is the base58encoded bitcoin address.
+        ///
+        public Address(string address)
+        {
+            //s_fromBase58(address);
+        }
+
+
+    }
+}
