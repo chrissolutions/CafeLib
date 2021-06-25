ad47ac8923b8d41d1685feb4f3220dd9ccf72c7b
chrissolutions <chris@chrissolutions.com>
2021-06-26 06:29:02 +1200

    Update ChainCollection to use KeyedCollection

--- Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp/Transactions/ChainCollection.cs
+++ Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp/Transactions/ChainCollection.cs
@@ -1,23 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 using CafeLib.BsvSharp.Numerics;
 
 namespace CafeLib.BsvSharp.Transactions
 {
-    public class ChainCollection<T> : IEnumerable<T> where T : IChainId
+    /// <summary>
+    ///	Collection of vertices.
+    /// </summary>
+    public class ChainCollection<T> : KeyedCollection<UInt256, T> where T : IChainId
     {
-        private readonly IDictionary<UInt256, T> _map = new ConcurrentDictionary<UInt256, T>();
+        #region Methods
 
-        public IEnumerator<T> GetEnumerator() => _map.Values.GetEnumerator();
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        public void Add(T item) => _map.Add(item.Hash, item);
-        public void Clear() => _map.Clear();
-        public bool Contains(T item) => _map.ContainsKey(item.Hash);
-        public int Count => _map.Count;
-        public void CopyTo(T[] array, int arrayIndex) => _map.Values.CopyTo(array, arrayIndex);
-        public bool Remove(T item) => _map.Remove(item.Hash);
-        public T this[UInt256 key] => _map[key];
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item">item to be added to the collection item</param>
+        public new void Add(T item)
+        {
+            if (!Contains(item))
+                base.Add(item);
+        }
+
+        /// <summary>
+        /// Determines whether the collection contains the item.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public new bool Contains(T item)
+        {
+            return Contains(item.Hash);
+        }
+
+        /// <summary>
+        /// Remove item from collection
+        /// </summary>
+        /// <param name="item">collection item</param>
+        /// <returns></returns>
+        public new bool Remove(T item)
+        {
+            return Contains(item.Hash) && Remove(item.Hash);
+        }
+
+        #endregion
+
+        #region Overrides
+
+        /// <summary>
+        /// Return the key of the keyed item.
+        /// </summary>
+        /// <param name="item">collection item</param>
+        /// <returns>the key of the item</returns>
+        protected override UInt256 GetKeyForItem(T item)
+        {
+            return item.Hash;
+        }
+
+        #endregion
     }
 
     public class TxInCollection : ChainCollection<TransactionInput>
