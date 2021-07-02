e45b8f1707858e7f38dd626b0689e06de91e2f73
chrissolutions <chris@chrissolutions.com>
2021-07-03 07:15:43 +1200

    Added ISignatureChecker interface.

---
+++ Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp/Transactions/ISignatureChecker.cs
@@ -0,0 +1,19 @@
+﻿#region Copyright
+// Copyright (c) 2020 TonesNotes
+// Distributed under the Open BSV software license, see the accompanying file LICENSE.
+#endregion
+
+using CafeLib.BsvSharp.Numerics;
+using CafeLib.BsvSharp.Scripting;
+
+namespace CafeLib.BsvSharp.Transactions
+{
+    public interface ISignatureChecker
+    {
+        bool CheckSignature(VarType scriptSig, VarType vchPubKey, Script script, ScriptFlags flags);
+
+        bool CheckLockTime(ScriptNum nLockTime);
+
+        bool CheckSequence(ScriptNum nSequence);
+    }
+}
