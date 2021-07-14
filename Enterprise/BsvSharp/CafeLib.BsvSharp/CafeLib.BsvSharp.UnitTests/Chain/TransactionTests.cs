7e3850fda20d5ac98f22d3cc41185dc1c5ebb241
chrissolutions <chris@chrissolutions.com>
2021-07-14 16:28:51 +1200

    Implementing Transaction test parser

--- Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp.UnitTests/Chain/TransactionTests.cs
+++ Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp.UnitTests/Chain/TransactionTests.cs
@@ -3,15 +3,47 @@
 // Distributed under the Open BSV software license, see the accompanying file LICENSE.
 #endregion
 
+using System;
+using System.Collections.Generic;
+using System.IO;
 using CafeLib.BsvSharp.Builders;
 using CafeLib.BsvSharp.Chain;
 using CafeLib.BsvSharp.Scripting;
+using Microsoft.VisualBasic.CompilerServices;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace CafeLib.BsvSharp.UnitTests.Chain
 {
     public class TransactionTests
     {
+        private const string DataFolder = @"..\..\..\data";
+        private static IEnumerable<string> GetValidTransaction()
+        {
+            var values = new List<string>();
+            var jarray = JArray.Parse(File.ReadAllText(Path.Combine(DataFolder, "tx_valid.json")));
+
+            foreach (var json in jarray)
+            {
+                values.Add(FindValueInArray(json));
+            }
+
+            return values;
+
+            static string FindValueInArray(JToken token)
+            {
+                if (!(token is JArray items)) return string.Empty;
+
+                foreach (var json in items)
+                {
+                    return json.Type == JTokenType.Array ? FindValueInArray((JArray)json) : json.Value<string>();
+                }
+
+                return string.Empty;
+            }
+        }
+
+
         private TransactionBuilder BuildCreditingTransaction(Script scriptPubKey, long nValue)
         {
             var tx = new TransactionBuilder
@@ -41,7 +73,8 @@ namespace CafeLib.BsvSharp.UnitTests.Chain
         [Fact]
         public void Basic()
         {
-
+            var lines = GetValidTransaction();
+            Console.WriteLine(lines);
         }
     }
 }
