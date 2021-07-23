03e3480c9d8d406b93fcbc57dda518b96981ff56
chrissolutions <chris@chrissolutions.com>
2021-07-24 09:15:50 +1200

    Added IsTxDerSignatureEncoding to Signature.cs

--- Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp/Scripting/ScriptEvaluator.cs
+++ Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp/Scripting/ScriptEvaluator.cs
@@ -910,7 +910,7 @@ namespace CafeLib.BsvSharp.Scripting
             if (vchSig.Length == 0) return true;
 
             if ((flags & (ScriptFlags.VERIFY_DERSIG | ScriptFlags.VERIFY_LOW_S | ScriptFlags.VERIFY_STRICTENC)) != 0
-                && !IsValidSignatureEncoding(vchSig))
+                && !Signature.IsTxDerSignatureEncoding(vchSig))
             {
                 return SetError(out error, ScriptError.SIG_DER);
             }
@@ -936,98 +936,12 @@ namespace CafeLib.BsvSharp.Scripting
 
         private static bool IsLowDerSignature(VarType vchSig, ref ScriptError error)
         {
-            if (!IsValidSignatureEncoding(vchSig)) return SetError(out error, ScriptError.SIG_DER);
+            if (!Signature.IsTxDerSignatureEncoding(vchSig)) return SetError(out error, ScriptError.SIG_DER);
 
             var sigInput = vchSig[..^1];
 
-            return Signature.CheckLowS(sigInput) || SetError(out error, ScriptError.SIG_HIGH_S);
+            return Signature.IsLowS(sigInput) || SetError(out error, ScriptError.SIG_HIGH_S);
         }
-
-        /**
-         * A canonical signature exists of: &lt;30&gt; &lt;total len&gt; &lt;02&gt; &lt;len R&gt; &lt;R&gt; &lt;02&gt;
-         * &lt;len S&gt; &lt;S&gt; &lt;hashtype&gt;, where R and S are not negative (their first byte has its
-         * highest bit not set), and not excessively padded (do not start with a 0 byte,
-         * unless an otherwise negative number follows, in which case a single 0 byte is
-         * necessary and even required).
-         *
-         * See https://bitcointalk.org/index.php?topic=8392.msg127623#msg127623
-         *
-         * This function is consensus-critical since BIP66.
-         */
-        private static bool IsValidSignatureEncoding(VarType vchSig)
-        {
-            // Format: 0x30 [total-length] 0x02 [R-length] [R] 0x02 [S-length] [S]
-            // [sighash]
-            // * total-length: 1-byte length descriptor of everything that follows,
-            // excluding the sighash byte.
-            // * R-length: 1-byte length descriptor of the R value that follows.
-            // * R: arbitrary-length big-endian encoded R value. It must use the
-            // shortest possible encoding for a positive integers (which means no null
-            // bytes at the start, except a single one when the next byte has its
-            // highest bit set).
-            // * S-length: 1-byte length descriptor of the S value that follows.
-            // * S: arbitrary-length big-endian encoded S value. The same rules apply.
-            // * sighash: 1-byte value indicating what data is hashed (not part of the
-            // DER signature)
-
-            // Minimum and maximum size constraints.
-            var length = vchSig.Length;
-            if (length < 9) return false;
-            if (length > 73) return false;
-
-            ReadOnlyByteSpan sig = vchSig;
-
-            // A signature is of type 0x30 (compound).
-            if (sig[0] != 0x30) return false;
-
-            // Make sure the length covers the entire signature.
-            if (sig[1] != sig.Length - 3) return false;
-
-            // Extract the length of the R element.
-            var lenR = sig[3];
-
-            // Make sure the length of the S element is still inside the signature.
-            if (5 + lenR >= sig.Length) return false;
-
-            // Extract the length of the S element.
-            var lenS = sig[5 + lenR];
-
-            // Verify that the length of the signature matches the sum of the length
-            // of the elements.
-            if (lenR + lenS + 7 != sig.Length) return false;
-
-            // Check whether the R element is an integer.
-            if (sig[2] != 0x02) return false;
-
-            // Zero-length integers are not allowed for R.
-            if (lenR == 0) return false;
-
-            // Negative numbers are not allowed for R.
-            if ((sig[4] & 0x80) != 0) return false;
-
-            // Null bytes at the start of R are not allowed, unless R would otherwise be
-            // interpreted as a negative number.
-            if (lenR > 1 && (sig[4] == 0) && (sig[5] & 0x80) == 0) return false;
-
-            // Check whether the S element is an integer.
-            if (sig[lenR + 4] != 0x02) return false;
-
-            // Zero-length integers are not allowed for S.
-            if (lenS == 0) return false;
-
-            // Negative numbers are not allowed for S.
-            if ((sig[lenR + 6] & 0x80) != 0) return false;
-
-            // Null bytes at the start of S are not allowed, unless S would otherwise be
-            // interpreted as a negative number.
-            if (lenS > 1 && (sig[lenR + 6] == 0x00) && (sig[lenR + 7] & 0x80) == 0)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static bool SetSuccess(out ScriptError ret)
--- Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp/Transactions/Signature.cs
+++ Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp/Transactions/Signature.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using CafeLib.Core.Buffers;
+﻿using CafeLib.Core.Buffers;
 using Secp256k1Net;
 
 namespace CafeLib.BsvSharp.Transactions
@@ -11,16 +8,94 @@ namespace CafeLib.BsvSharp.Transactions
         private const int SignatureSize = 64;
         
         /// <summary>
-        /// Check whether a signature is normalized (lower-S).
+        /// Determine whether a signature is normalized (lower-S).
         /// </summary>
-        /// <param name="vchSig"></param>
+        /// <param name="signature"></param>
         /// <returns></returns>
-        public static bool CheckLowS(ReadOnlyByteSpan vchSig)
+        public static bool IsLowS(ReadOnlyByteSpan signature)
         {
-            var sig = new byte[SignatureSize];
+            var sigOut = new byte[SignatureSize];
             using var library = new Secp256k1();
-            if (!library.SignatureParseDerLax(sig, vchSig)) return false;
-            return !library.SignatureNormalize(sig, vchSig);
+            if (!library.SignatureParseDerLax(sigOut, signature)) return false;
+            return !library.SignatureNormalize(sigOut, signature);
+        }
+
+        /**
+         * A canonical signature exists of: &lt;30&gt; &lt;total len&gt; &lt;02&gt; &lt;len R&gt; &lt;R&gt; &lt;02&gt;
+         * &lt;len S&gt; &lt;S&gt; &lt;hashtype&gt;, where R and S are not negative (their first byte has its
+         * highest bit not set), and not excessively padded (do not start with a 0 byte,
+         * unless an otherwise negative number follows, in which case a single 0 byte is
+         * necessary and even required).
+         *
+         * See https://bitcointalk.org/index.php?topic=8392.msg127623#msg127623
+         *
+         * This function is consensus-critical since BIP66.
+         */
+        public static bool IsTxDerSignatureEncoding(ReadOnlyByteSpan signature)
+        {
+            // Format: 0x30 [total-length] 0x02 [R-length] [R] 0x02 [S-length] [S]
+            // [sighash]
+            // * total-length: 1-byte length descriptor of everything that follows,
+            // excluding the sighash byte.
+            // * R-length: 1-byte length descriptor of the R value that follows.
+            // * R: arbitrary-length big-endian encoded R value. It must use the
+            // shortest possible encoding for a positive integers (which means no null
+            // bytes at the start, except a single one when the next byte has its
+            // highest bit set).
+            // * S-length: 1-byte length descriptor of the S value that follows.
+            // * S: arbitrary-length big-endian encoded S value. The same rules apply.
+            // * sighash: 1-byte value indicating what data is hashed (not part of the
+            // DER signature)
+
+            // Minimum and maximum size constraints.
+            var length = signature.Length;
+            if (length < 9) return false;
+            if (length > 73) return false;
+
+            // A signature is of type 0x30 (compound).
+            if (signature[0] != 0x30) return false;
+
+            // Make sure the length covers the entire signature.
+            if (signature[1] != signature.Length - 3) return false;
+
+            // Extract the length of the R element.
+            var lenR = signature[3];
+
+            // Make sure the length of the S element is still inside the signature.
+            if (5 + lenR >= signature.Length) return false;
+
+            // Extract the length of the S element.
+            var lenS = signature[5 + lenR];
+
+            // Verify that the length of the signature matches the sum of the length
+            // of the elements.
+            if (lenR + lenS + 7 != signature.Length) return false;
+
+            // Check whether the R element is an integer.
+            if (signature[2] != 0x02) return false;
+
+            // Zero-length integers are not allowed for R.
+            if (lenR == 0) return false;
+
+            // Negative numbers are not allowed for R.
+            if ((signature[4] & 0x80) != 0) return false;
+
+            // Null bytes at the start of R are not allowed, unless R would otherwise be
+            // interpreted as a negative number.
+            if (lenR > 1 && (signature[4] == 0) && (signature[5] & 0x80) == 0) return false;
+
+            // Check whether the S element is an integer.
+            if (signature[lenR + 4] != 0x02) return false;
+
+            // Zero-length integers are not allowed for S.
+            if (lenS == 0) return false;
+
+            // Negative numbers are not allowed for S.
+            if ((signature[lenR + 6] & 0x80) != 0) return false;
+
+            // Null bytes at the start of S are not allowed, unless S would otherwise be
+            // interpreted as a negative number.
+            return lenS <= 1 || (signature[lenR + 6] != 0x00) || (signature[lenR + 7] & 0x80) != 0;
         }
 
         //final ECDomainParameters _domainParams =  ECDomainParameters('secp256k1');
