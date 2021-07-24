3ea3eceaca201448fe7d15aefab1792394ec2103
chrissolutions <chris@chrissolutions.com>
2021-07-24 14:21:38 +1200

    Added P2PkhLockBuilder & P2PkhUnlockBuilder classes

---
+++ Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp/Transactions/P2PkhLockBuilder.cs
@@ -0,0 +1,25 @@
+﻿using CafeLib.BsvSharp.Builders;
+using CafeLib.BsvSharp.Keys;
+using CafeLib.BsvSharp.Numerics;
+using CafeLib.BsvSharp.Scripting;
+
+namespace CafeLib.BsvSharp.Transactions
+{
+    public class P2PkhLockBuilder : ScriptBuilder
+    {
+        public P2PkhLockBuilder(UInt160 pubKeyHash)
+            :base(true, TemplateId.Pay2PublicKeyHash)
+        {
+            Add(Opcode.OP_DUP)
+                .Add(Opcode.OP_HASH160)
+                .Push(pubKeyHash.Span)
+                .Add(Opcode.OP_EQUALVERIFY)
+                .Add(Opcode.OP_CHECKSIG);
+        }
+
+        public P2PkhLockBuilder(PublicKey publicKey)
+            : this(publicKey.ToHash160())
+        {
+        }
+    }
+}
---
+++ Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp/Transactions/P2PkhUnlockBuilder.cs
@@ -0,0 +1,21 @@
+﻿#region Copyright
+// Copyright (c) 2020 TonesNotes
+// Distributed under the Open BSV software license, see the accompanying file LICENSE.
+#endregion
+
+using CafeLib.BsvSharp.Builders;
+using CafeLib.BsvSharp.Keys;
+using CafeLib.BsvSharp.Scripting;
+
+namespace CafeLib.BsvSharp.Transactions
+{
+    public class P2PkhUnlockBuilder : ScriptBuilder
+    {
+        public P2PkhUnlockBuilder(PublicKey pubKey)
+            : base(false, TemplateId.Pay2PublicKeyHash)
+        {
+            Push(new byte[72]) // This will become the CHECKSIG signature
+                .Push(pubKey);
+        }
+    }
+}
