4de769fbdba95fa5f751d9637fbb9270fdb70ec0
chrissolutions <chris@chrissolutions.com>
2021-08-02 10:20:01 +1200

    Removed unused classes.

--- Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp/Transactions/IChainDataInput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CafeLib.BsvSharp.Transactions
-{
-    public interface IChainDataInput<out T>
-    {
-        T ParseBuffer(byte[] buffer);
-        T ParseHex(string hex);
-        T ParseJson(string json);
-    }
-}
