3ef952fa8e847d239e7957103dba9a85f477e3c6
chrissolutions <chris@chrissolutions.com>
2021-08-07 22:14:30 +1200

    Working on Verify_Script_From_Simple_Transaction

--- Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp.UnitTests/Scripts/ScriptInterpreterTests.cs
+++ Enterprise/BsvSharp/CafeLib.BsvSharp/CafeLib.BsvSharp.UnitTests/Scripts/ScriptInterpreterTests.cs
@@ -1,7 +1,11 @@
 ﻿using CafeLib.BsvSharp.Builders;
 using CafeLib.BsvSharp.Encoding;
+using CafeLib.BsvSharp.Keys;
+using CafeLib.BsvSharp.Network;
 using CafeLib.BsvSharp.Numerics;
 using CafeLib.BsvSharp.Scripting;
+using CafeLib.BsvSharp.Services;
+using CafeLib.BsvSharp.Signatures;
 using CafeLib.BsvSharp.Transactions;
 using CafeLib.BsvSharp.Units;
 using Xunit;
@@ -40,38 +44,38 @@ namespace CafeLib.BsvSharp.UnitTests.Scripts
             Assert.Equal(new VarType(Encoders.Hex.Decode(value)), result);
         }
 
-        //[Fact]
-        //public void Verify_Script_From_Simple_Transaction()
-        //{
-        //    RootService.Bootstrap(NetworkType.Test);
-        //    var privateKey = PrivateKey.FromWif("cSBnVM4xvxarwGQuAfQFwqDg9k5tErHUHzgWsEfD4zdwUasvqRVY");
-        //    var publicKey = privateKey.CreatePublicKey();
-        //    var fromAddress = publicKey.ToAddress();
-        //    var toAddress = new Address("mrU9pEmAx26HcbKVrABvgL7AwA5fjNFoDc");
-        //    var scriptPubkey = new P2PkhLockBuilder(fromAddress).ToScript();
+        [Fact]
+        public void Verify_Script_From_Simple_Transaction()
+        {
+            RootService.Bootstrap(NetworkType.Test);
+            var privateKey = PrivateKey.FromWif("cSBnVM4xvxarwGQuAfQFwqDg9k5tErHUHzgWsEfD4zdwUasvqRVY");
+            var publicKey = privateKey.CreatePublicKey();
+            var fromAddress = publicKey.ToAddress();
+            var toAddress = new Address("mrU9pEmAx26HcbKVrABvgL7AwA5fjNFoDc");
+            var scriptPubkey = new P2PkhLockBuilder(fromAddress).ToScript();
 
-        //    var utxo = new Utxo
-        //    {
-        //        TxHash = new UInt256("a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458"),
-        //        Index = 0,
-        //        ScriptPubKey = scriptPubkey,
-        //        Amount = 100000
-        //    };                    
+            var utxo = new Utxo
+            {
+                TxHash = new UInt256("a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458"),
+                Index = 0,
+                ScriptPubKey = scriptPubkey,
+                Amount = 100000
+            };
 
-        //    var tx = new Transaction();
-        //    tx.SpendFromUtxo(utxo, new P2PkhUnlockBuilder(publicKey));
-        //    tx.SpendTo(toAddress, 100000L, new P2PkhLockBuilder(toAddress));
-        //    tx.SignInput(0, privateKey, SignatureHashEnum.All);
-            
-        //    // we then extract the signature from the first input
-        //    var scriptSig = tx.Inputs[0].ScriptSig;
+            var tx = new Transaction();
+            tx.SpendFromUtxo(utxo, new P2PkhUnlockBuilder(publicKey));
+            tx.SpendTo(toAddress, 100000L, new P2PkhLockBuilder(toAddress));
+            tx.SignInput(0, privateKey, SignatureHashEnum.All);
 
-        //    var scriptBuilder = new P2PkhUnlockBuilder(publicKey);
+            // we then extract the signature from the first input
+            var scriptSig = tx.Inputs[0].ScriptSig;
 
-        //    var flags = ScriptFlags.VERIFY_P2SH | ScriptFlags.VERIFY_STRICTENC;
-        //    var checker = new TransactionSignatureChecker(tx, 0, utxo.Amount);
-        //    var verified = ScriptInterpreter.VerifyScript(scriptSig, scriptPubkey, flags, checker, out var error);
-        //    Assert.True(verified);
-        //}
+            var scriptBuilder = new P2PkhUnlockBuilder(publicKey);
+
+            var flags = ScriptFlags.VERIFY_P2SH | ScriptFlags.VERIFY_STRICTENC;
+            var checker = new TransactionSignatureChecker(tx, 0, utxo.Amount);
+            var verified = ScriptInterpreter.VerifyScript(scriptSig, scriptPubkey, flags, checker, out var error);
+            Assert.True(verified);
+        }
     }
 }
