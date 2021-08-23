2acdf38bf1abb7b766452d8a8b6960c7fcc504f6
chrissolutions <chris@chrissolutions.com>
2021-08-23 16:24:50 +1200

    Renaming KzSecp256k1 to CafeLib.Secp256k1

---
+++ Enterprise/BsvSharp/libs/CafeLib.Secp256k1/DynamicLinking/DynamicLinkingWindows.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Secp256k1Net.DynamicLinking
+{
+    internal static class DynamicLinkingWindows
+    {
+        private const string Kernel32 = "kernel32";
+
+        [DllImport(Kernel32, SetLastError = true)]
+        public static extern IntPtr LoadLibrary(string path);
+
+        [DllImport(Kernel32, SetLastError = true)]
+        public static extern int FreeLibrary(IntPtr module);
+
+        [DllImport(Kernel32, SetLastError = true, CharSet = CharSet.Ansi, ExactSpelling = true)]
+        public static extern IntPtr GetProcAddress(IntPtr module, string procName);
+    }
+}
