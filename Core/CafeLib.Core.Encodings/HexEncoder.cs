b965a24ee60c7571f449c2a22b28b98ce708e342
chrissolutions <chris@chrissolutions.com>
2021-09-13 15:03:03 +1200

    Added HexEncoder & HexReverseEncoder classes to CafeLib.Core.Encodings

---
+++ Core/CafeLib.Core.Encodings/HexEncoder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using CafeLib.Core.Buffers;
+
+namespace CafeLib.Core.Encodings
+{
+    /// <summary>
+    /// The string begins with the first byte.
+    /// Encodes a sequence of bytes as hexadecimal digits where:
+    /// Character 0 corresponds to the high nibble of the first byte. 
+    /// Character 1 corresponds to the low nibble of the first byte. 
+    /// </summary>
+    public class HexEncoder : IEncoder
+    {
+        private const int NumericDigits = '9' - '0' + 1;
+        private const int AlphaDigits = 'f' - 'a' + 1;
+        protected static readonly string[] ByteToChs = Enumerable.Range(0, 256).Select(v => v.ToString("x2")).ToArray();
+        protected static readonly int[] CharToNibbleArray = new int['f' + 1];
+
+        static HexEncoder()
+        {
+            for (var i = 0; i < 'a'; ++i) CharToNibbleArray[i] = -1;
+            for (var i = 0; i < NumericDigits; ++i) CharToNibbleArray[i + '0'] = i;
+            for (var i = 0; i < AlphaDigits; ++i)
+            {
+                CharToNibbleArray[i + 'a'] = i + NumericDigits;
+                CharToNibbleArray[i + 'A'] = i + NumericDigits;
+            }
+        }
+
+        public string Encode(byte[] source) => EncodeSpan(source);
+
+        public byte[] Decode(string source) => TryDecode(source, out var bytes) ? bytes : throw new FormatException(nameof(source));
+
+        public bool TryDecode(string hex, out byte[] bytes)
+        {
+            hex = hex[hex.StartsWith("0x") ? 2.. : ..];
+            bytes = new byte[hex.Length / 2];
+            var span = bytes.AsSpan();
+            return TryDecodeSpan(hex, span);
+        }
+
+        internal virtual string EncodeSpan(ReadOnlyByteSpan bytes)
+        {
+            var s = new char[bytes.Length * sizeof(char)];
+            var i = 0;
+            foreach (var b in bytes)
+            {
+                var chs = ByteToChs[b];
+                s[i++] = chs[0];
+                s[i++] = chs[1];
+            }
+            return new string(s);
+        }
+
+        internal virtual bool TryDecodeSpan(string hex, ByteSpan bytes)
+        {
+            if (hex.Length % 2 == 1 || hex.Length / 2 > bytes.Length)
+                return false;
+
+            if (hex.Length / 2 < bytes.Length)
+                bytes[(hex.Length / 2)..].Data.Fill(0);
+
+            for (int i = 0, j = 0; i < hex.Length;)
+            {
+                var a = CharToNibble(hex[i++]);
+                var b = CharToNibble(hex[i++]);
+                if (a == -1 || b == -1) return false;
+                bytes[j++] = (byte)((a << 4) | b);
+            }
+
+            return true;
+        }
+
+        protected static int CharToNibble(char c)
+        {
+            return c > CharToNibbleArray.Length ? -1 : CharToNibbleArray[c];
+        }
+    }
+}
--- Core/CafeLib.Core.UnitTests/EncodingsTests.cs
+++ Core/CafeLib.Core.UnitTests/EncodingsTests.cs
@@ -9,25 +9,25 @@ namespace CafeLib.Core.UnitTests
         [Fact]
         public void Little_Endian_Test()
         {
-            var bytes = new ByteSpan(new byte[] { 0xAC, 0x1E, 0xED, 0x88 });
+            var bytes = new ByteSpan(new byte[] {0xAC, 0x1E, 0xED, 0x88});
             var endianEncoder = new EndianEncoder();
 
             var le = endianEncoder.LittleEndianInt32(bytes);
-            Assert.Equal(unchecked((int)(0x88ED1EAC)), le);
+            Assert.Equal(unchecked((int) (0x88ED1EAC)), le);
         }
 
         [Fact]
         public void Big_Endian_Test()
         {
             var endianEncoder = new EndianEncoder();
-            var bytes = new ByteSpan(new byte[] { 0xAC, 0x1E, 0xED, 0x88 });
+            var bytes = new ByteSpan(new byte[] {0xAC, 0x1E, 0xED, 0x88});
 
             var be = endianEncoder.BigEndianInt32(bytes);
-            Assert.Equal(unchecked((int)(0xAC1EED88)), be);
+            Assert.Equal(unchecked((int) (0xAC1EED88)), be);
         }
 
         [Fact]
-        public void Decode_Encode_Test()
+        public void Endian_Decode_Encode_Test()
         {
             const string message = "Kilroy was here!";
             var endianEncoder = new EndianEncoder();
@@ -36,6 +36,31 @@ namespace CafeLib.Core.UnitTests
             var decoded = endianEncoder.Encode(encodedBytes);
 
             Assert.Equal(message, decoded);
+        }
+
+        [Fact]
+        public void Hex_Forward_Encode_Decode_Test()
+        {
+            var hex = new HexEncoder();
+
+            var b0 = new byte[] {0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0};
+            const string s0 = "123456789abcdef0";
+            const string s0U = "123456789ABCDEF0";
+
+            Assert.Equal(s0, hex.Encode(b0));
+            Assert.Equal(b0, hex.Decode(s0));
+            Assert.Equal(b0, hex.Decode(s0U));
+        }
+
+        [Fact]
+        public void Hex_Reverse_Encode_Decode_Test()
+        {
+            var hexReverse = new HexReverseEncoder();
+
+            var b0R = new byte[] {0x10, 0x32, 0x54, 0x76, 0x98, 0xba, 0xdc, 0xfe};
+            const string s0R = "fedcba9876543210";
+            Assert.Equal(s0R, hexReverse.Encode(b0R));
+            Assert.Equal(b0R, hexReverse.Decode(s0R));
         }
     }
 }
