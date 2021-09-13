b15438eb6eb7d1887a45ce5760cccb78a7301e99
chrissolutions <chris@chrissolutions.com>
2021-09-13 17:54:01 +1200

    Setting up CafeLib.Core.Numerics

--- Core/CafeLib.Core.Numerics/UInt512.cs
+++ Core/CafeLib.Core.Numerics/UInt512.cs
@@ -1,9 +1,4 @@
-﻿#region Copyright
-// Copyright (c) 2020 TonesNotes
-// Distributed under the Open BSV software license, see the accompanying file LICENSE.
-#endregion
-
-using System;
+﻿using System;
 using System.IO;
 using System.Numerics;
 
