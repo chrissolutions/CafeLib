cbdd1f7708f82388abd221be12a734e0ad8b57e1
chrissolutions <chris@chrissolutions.com>
2021-09-28 08:02:18 +1300

    Setting up BsvSharp.Api build.cmd

--- Enterprise/BsvSharp.Api/build.cmd
+++ Enterprise/BsvSharp.Api/build.cmd
@@ -3,33 +3,23 @@ setlocal
 if '%root%' == '' set root=..
 
 :: Type
-set type=Core
+:: Type
+set type=BsvSharp.Api
+set location=Enterprise\%type%
 
 :: Settings
 call %root%\build\buildenv %*
 if ERRORLEVEL 1 goto error
 set solution=CafeLib.%type%
-set sourcepath=%root%\%type%
+set sourcepath=%root%\%location%
 
 :: Setup libraries.
 set libs=%solution%
-set libs=%libs% %solution%.Buffers 
-set libs=%libs% %solution%.Caching 
-set libs=%libs% %solution%.Collections
-set libs=%libs% %solution%.Collections.DirectedGraph
-set libs=%libs% %solution%.Data 
-set libs=%libs% %solution%.Dynamic
-set libs=%libs% %solution%.Encodings
-set libs=%libs% %solution%.Eventing
-set libs=%libs% %solution%.FileIO
-set libs=%libs% %solution%.IoC
-set libs=%libs% %solution%.Logging
-set libs=%libs% %solution%.Messaging
-set libs=%libs% %solution%.MethodBinding
-set libs=%libs% %solution%.Numerics
-set libs=%libs% %solution%.Queueing
-set libs=%libs% %solution%.Runnable
-set libs=%libs% %solution%.Security
+set libs=%libs% %solution%.
+set libs=%libs% %solution%.CoinGecko
+set libs=%libs% %solution%.CoinMarketCap
+set libs=%libs% %solution%.Paymail
+set libs=%libs% %solution%.WhatsOnChain
 ::
 
 :: Run script to build the libraries
