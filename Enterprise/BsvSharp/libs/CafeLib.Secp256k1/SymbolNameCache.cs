8e06065dbc95d5fbb176beb13db041a356577a20
chrissolutions <chris@chrissolutions.com>
2021-09-27 22:37:36 +1300

    Removing CafeLib.Secp256k1; Adding READMEs; Adding Api build script.

--- Enterprise/BsvSharp/libs/CafeLib.Secp256k1/SymbolNameCache.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-namespace Secp256k1Net
-{
-    internal static class SymbolNameCache<TDelegate>
-    {
-        public static string GetSymbolName() => typeof(TDelegate).GetCustomAttribute<SymbolNameAttribute>().Name;
-    }
-}
