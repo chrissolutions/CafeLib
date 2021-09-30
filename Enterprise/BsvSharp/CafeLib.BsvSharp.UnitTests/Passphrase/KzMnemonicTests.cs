634f7ec5d014187f8ee44e6cd74cf43a9c9fd11a
chrissolutions <chris@chrissolutions.com>
2021-09-30 20:04:39 +1300

    Removed extraneous references in KzMnemonicTests

--- Enterprise/BsvSharp/CafeLib.BsvSharp.UnitTests/Passphrase/KzMnemonicTests.cs
+++ Enterprise/BsvSharp/CafeLib.BsvSharp.UnitTests/Passphrase/KzMnemonicTests.cs
@@ -3,7 +3,6 @@
 // Distributed under the Open BSV software license, see the accompanying file LICENSE.
 #endregion
 
-using System.Collections.Generic;
 using System.Linq;
 using CafeLib.BsvSharp.Keys;
 using CafeLib.BsvSharp.Passphrase;
