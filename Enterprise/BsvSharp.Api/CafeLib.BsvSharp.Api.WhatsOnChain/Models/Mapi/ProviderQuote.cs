66473616f5f01dae8fae47784941a9d3c023edf0
chrissolutions <chris@chrissolutions.com>
2021-10-06 21:57:34 +1300

    Working on Mapi

---
+++ Enterprise/BsvSharp.Api/CafeLib.BsvSharp.Api.WhatsOnChain/Models/Mapi/ProviderQuote.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace CafeLib.BsvSharp.Api.WhatsOnChain.Models.Mapi
+{
+    public class ProviderQuote
+    {
+        [JsonProperty("providerName")]
+        public string ProviderName { get; set; }
+
+        [JsonProperty("providerId")]
+        public string ProviderId { get; set; }
+
+        [JsonProperty("quote")]
+        public Quote Quote { get; set; }
+
+        [JsonProperty("encoding")]
+        public string Encoding { get; set; }
+
+        [JsonProperty("mimetype")]
+        public string MimeType { get; set; }
+    }
+}
---
+++ Enterprise/BsvSharp.Api/CafeLib.BsvSharp.Api.WhatsOnChain/Models/Mapi/Quotes.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace CafeLib.BsvSharp.Api.WhatsOnChain.Models.Mapi
+{
+    public class Quotes
+    {
+        [JsonProperty("quotes")]
+        public ProviderQuote[] ProviderQuotes { get; set; }
+
+        [JsonProperty("signature")]
+        public string Signature { get; set; }
+
+        [JsonProperty("publicKey")]
+        public string PublicKey { get; set; }
+
+        [JsonProperty("encoding")]
+        public string Encoding { get; set; }
+
+        [JsonProperty("mimetype")]
+        public string MimeType { get; set; }
+    }
+}
