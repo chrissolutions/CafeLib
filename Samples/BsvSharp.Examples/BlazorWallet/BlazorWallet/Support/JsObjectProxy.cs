7c1305c8a93e198b05d84da6c777de8317485a64
chrissolutions <chris@chrissolutions.com>
2021-10-11 16:22:33 +1300

    Added Samples folder; copied BlazorWallet to Samples.

---
+++ Samples/BsvSharp.Examples/BlazorWallet/BlazorWallet/Support/JsObjectProxy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using CafeLib.Core.Support;
+using Microsoft.JSInterop;
+
+namespace BlazorWallet.Support
+{
+    public class JsObjectProxy
+    {
+        private readonly LazyAsync<IJSObjectReference> _proxy;
+        private readonly string _createMethod;
+
+        public JsObjectProxy(string proxyScript, IJSRuntime jsRuntime)
+            : this(proxyScript, null, jsRuntime)
+        {
+        }
+
+        public JsObjectProxy(string factoryScript, string createMethod, IJSRuntime jsRuntime)
+        {
+            factoryScript = !string.IsNullOrWhiteSpace(factoryScript) ? factoryScript : throw new ArgumentNullException(nameof(factoryScript));
+            _proxy = new LazyAsync<IJSObjectReference>(async () => await jsRuntime.Import<IJSObjectReference>(factoryScript));
+            _createMethod = !string.IsNullOrWhiteSpace(createMethod) ? createMethod : "create";
+        }
+
+        public async Task<IJSObjectReference> CreateReferenceAsync(params object[] args)
+        {
+            return await (await _proxy).InvokeAsync<IJSObjectReference>(_createMethod, args);
+        }
+    }
+}
