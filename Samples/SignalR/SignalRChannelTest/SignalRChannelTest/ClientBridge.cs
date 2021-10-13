9eb2c00f015781a86bd9b1e002a5e6dc38aebbdf
chrissolutions <chris@chrissolutions.com>
2021-10-13 20:51:55 +1300

    Added SignalR example

---
+++ Samples/SignalR/SignalRChannelTest/SignalRChannelTest/ClientBridge.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CafeLib.Core.MethodBinding;
+
+namespace SignalRChannelTest
+{
+    public sealed class ClientBridge : MethodBridge
+    {
+        [MethodExport("doClientStuff")]
+        public void DoClientStuff(int arg1, string arg2)
+        {
+            Console.WriteLine($"doClientStuff: {arg1}: {arg2}");
+        }
+
+        [MethodExport("notifyMessage")]
+        public void NotifyMessage(string data)
+        {
+            Console.WriteLine($"Incoming data: {data}");
+        }
+
+        [MethodExport("doGuidTest")]
+        public void DoGuidTest(Guid arg1, string arg2)
+        {
+            Console.WriteLine($"doGuidTest: {arg1}: {arg2}");
+        }
+
+        /// <summary>
+        /// Override invoke to report argument exceptions
+        /// </summary>
+        /// <param name="method">export method name</param>
+        /// <param name="args">arguments</param>
+        public override void Invoke(string method, object[] args)
+        {
+            try
+            {
+                base.Invoke(method, args);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Calling export {method}:{Environment.NewLine}{ex}");
+            }
+        }
+    }
+}
---
+++ Samples/SignalR/SignalRChannelTest/SignalRChannelTest/TestLogWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CafeLib.Core.Logging;
+using CafeLib.Web.SignalR;
+
+namespace SignalRChannelTest
+{
+    public class TestLogWriter : LogEventWriter<SignalRChannel>
+    {
+        public TestLogWriter()
+            : base(LogWriterListener)
+        {
+        }
+
+        private static void LogWriterListener(LogEventMessage m)
+        {
+            Console.WriteLine($"Logging: {m.LogLevel}, {m.Category}, {m.Message}");
+        }
+    }
+}
