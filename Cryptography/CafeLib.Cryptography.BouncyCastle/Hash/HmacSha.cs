4bf19a318b45d6fe99ab343babf038cd869e07ea
chrissolutions <chris@chrissolutions.com>
2021-12-01 10:27:46 +1300

    Added HmacSha abstract class.

---
+++ Cryptography/CafeLib.Cryptography.BouncyCastle/Hash/HmacSha.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CafeLib.Cryptography.BouncyCastle.Crypto;
+using CafeLib.Cryptography.BouncyCastle.Crypto.Macs;
+using CafeLib.Cryptography.BouncyCastle.Crypto.Parameters;
+
+namespace CafeLib.Cryptography.BouncyCastle.Hash
+{
+    public abstract class HmacSha
+    {
+        private readonly HMac _hmac;
+
+        protected HmacSha(IDigest digest, byte[] key)
+        {
+            _hmac = new HMac(digest);
+            _hmac.Init(new KeyParameter(key));
+        }
+
+        public virtual byte[] ComputeHash(byte[] value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
+            var resBuf = new byte[_hmac.GetMacSize()];
+            _hmac.BlockUpdate(value, 0, value.Length);
+            _hmac.DoFinal(resBuf, 0);
+
+            return resBuf;
+        }
+    }
+}
--- Cryptography/CafeLib.Cryptography.BouncyCastle/Hash/HmacSha512.cs
+++ Cryptography/CafeLib.Cryptography.BouncyCastle/Hash/HmacSha512.cs
@@ -1,29 +1,12 @@
-﻿using System;
-using CafeLib.Cryptography.BouncyCastle.Crypto.Digests;
-using CafeLib.Cryptography.BouncyCastle.Crypto.Macs;
-using CafeLib.Cryptography.BouncyCastle.Crypto.Parameters;
+﻿using CafeLib.Cryptography.BouncyCastle.Crypto.Digests;
 
 namespace CafeLib.Cryptography.BouncyCastle.Hash
 {
-    public class HmacSha512
+    public class HmacSha512 : HmacSha
     {
-        private readonly HMac _hmac;
-
         public HmacSha512(byte[] key)
+            : base(new Sha512Digest(), key)
         {
-            _hmac = new HMac(new Sha512Digest());
-            _hmac.Init(new KeyParameter(key));
-        }
-
-        public byte[] ComputeHash(byte[] value)
-        {
-            if (value == null) throw new ArgumentNullException(nameof(value));
-
-            byte[] resBuf = new byte[_hmac.GetMacSize()];
-            _hmac.BlockUpdate(value, 0, value.Length);
-            _hmac.DoFinal(resBuf, 0);
-
-            return resBuf;
         }
     }
 }
