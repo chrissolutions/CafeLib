55fce1f42466de54f3a33093c23a7d15130ebefb
chrissolutions <chris@chrissolutions.com>
2021-12-01 13:33:00 +1300

    Added encryption tests.

--- Cryptography/CafeLib.Cryptography.BouncyCastle/Hash/HmacSha.cs
+++ Cryptography/CafeLib.Cryptography.BouncyCastle/Hash/HmacSha.cs
@@ -7,22 +7,21 @@ namespace CafeLib.Cryptography.BouncyCastle.Hash
 {
     public abstract class HmacSha
     {
-        private readonly HMac _hmac;
+        public HMac HMac { get; }
 
         protected HmacSha(IDigest digest, byte[] key)
         {
-            _hmac = new HMac(digest);
-            _hmac.Init(new KeyParameter(key));
+            HMac = new HMac(digest);
+            HMac.Init(new KeyParameter(key));
         }
 
         public virtual byte[] ComputeHash(byte[] value)
         {
             if (value == null) throw new ArgumentNullException(nameof(value));
 
-            var resBuf = new byte[_hmac.GetMacSize()];
-            _hmac.BlockUpdate(value, 0, value.Length);
-            _hmac.DoFinal(resBuf, 0);
-
+            var resBuf = new byte[HMac.GetMacSize()];
+            HMac.BlockUpdate(value, 0, value.Length);
+            HMac.DoFinal(resBuf, 0);
             return resBuf;
         }
     }
