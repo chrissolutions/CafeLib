cee5356d04de76a8ce5670ee688f2c8b9b2d0ca6
chrissolutions <chris@chrissolutions.com>
2021-12-04 23:08:15 +1300

    Working on CafeLib.Authorization.Security

---
+++ Authorization/CafeLib.Authorization.Security/BytesEqualityComparer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CafeLib.Authorization.Security
+{
+    internal class Class2
+    {
+    }
+}
--- Authorization/CafeLib.Authorization.Security/Class1.cs
+++ Authorization/CafeLib.Authorization.Security/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 using CafeLib.Cryptography.BouncyCastle.Crypto.Generators;
 using CafeLib.Cryptography.BouncyCastle.Crypto.Parameters;
@@ -48,5 +49,53 @@ namespace CafeLib.Authorization.Security
             // if the bytes of hashToCheck don't match the bytes of hash
             // that means the password is invalid
         }
-    }
+
+        /// <summary>
+        /// Hash password.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns>hash of the password</returns>
+        public string HashPassword(string password)
+        {
+            byte[] saltBuffer;
+            byte[] hashBuffer;
+
+            using (var keyDerivation = new Rfc2898DeriveBytes(password, _options.SaltSize, _options.Iterations, _hashName))
+            {
+                saltBuffer = keyDerivation.Salt;
+                hashBuffer = keyDerivation.GetBytes(_options.HashSize);
+            }
+
+            var result = new byte[_options.HashSize + _options.SaltSize];
+            Buffer.BlockCopy(hashBuffer, 0, result, 0, _options.HashSize);
+            Buffer.BlockCopy(saltBuffer, 0, result, _options.HashSize, _options.SaltSize);
+            return Convert.ToBase64String(result);
+        }
+
+        /// <summary>
+        /// Verify hashed password.
+        /// </summary>
+        /// <param name="hashedPassword"></param>
+        /// <param name="providedPassword"></param>
+        /// <returns></returns>
+        public bool VerifyHashedPassword(string hashedPassword, string providedPassword)
+        {
+            var hashedPasswordBytes = Convert.FromBase64String(hashedPassword);
+            if (hashedPasswordBytes.Length != _options.HashSize + _options.SaltSize)
+            {
+                return false;
+            }
+
+            var hashBytes = new byte[_options.HashSize];
+            Buffer.BlockCopy(hashedPasswordBytes, 0, hashBytes, 0, _options.HashSize);
+            var saltBytes = new byte[_options.SaltSize];
+            Buffer.BlockCopy(hashedPasswordBytes, _options.HashSize, saltBytes, 0, _options.SaltSize);
+
+            using var keyDerivation = new Rfc2898DeriveBytes(providedPassword, saltBytes, _options.Iterations, _hashName);
+            var providedHashBytes = keyDerivation.GetBytes(_options.HashSize);
+
+            return _comparer.Equals(hashBytes, providedHashBytes);
+        }
+
+	}
 }
