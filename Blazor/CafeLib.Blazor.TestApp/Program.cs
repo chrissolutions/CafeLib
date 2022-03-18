358e0c9604588e94fb509a0de42313bfef8626be
chrissolutions <chris@chrissolutions.com>
2022-03-18 17:28:23 +1300

    Added CafeLib.Blazor.TestApp project to solution.

--- Blazor/CafeLib.Blazor.TestApp/Program.cs
+++ Blazor/CafeLib.Blazor.TestApp/Program.cs
@@ -1,12 +1,10 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using CafeLib.Blazor.TestApp.Interop;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
 
 namespace CafeLib.Blazor.TestApp
 {
@@ -17,7 +15,8 @@ namespace CafeLib.Blazor.TestApp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new QrCodeProxy(sp.GetService<IJSRuntime>()));
 
             await builder.Build().RunAsync();
         }
