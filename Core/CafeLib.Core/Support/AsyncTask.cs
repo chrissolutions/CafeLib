21b84c16cedd1ad80792e6fed471f18ba0a97b4d
chrissolutions <chris@chrissolutions.com>
2022-04-13 16:25:36 +1200

    Added AsyncTask support to properly handle WhenAll exceptions.

---
+++ Core/CafeLib.Core/Support/AsyncTask.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+// ReSharper disable UnusedMember.Global
+
+namespace CafeLib.Core.Support
+{
+    public static class AsyncTask
+    {
+        /// <summary>
+        /// Task.WhenAll wrapper that returns an aggregate of all of the exceptions that occurs in a task collection.
+        /// </summary>
+        /// <typeparam name="T">collection type</typeparam>
+        /// <param name="tasks">collection of tasks</param>
+        /// <returns>enumerable of type T</returns>
+        public static async Task WhenAll(params Task[] tasks)
+        {
+            var allTasks = Task.WhenAll(tasks);
+
+            try
+            {
+                await allTasks;
+                return;
+            }
+            catch (Exception)
+            {
+                // ignore
+            }
+
+            throw allTasks.Exception ?? throw new Exception();
+        }
+
+        /// <summary>
+        /// Task.WhenAll wrapper that returns an aggregate of all of the exceptions that occurs in a task collection.
+        /// </summary>
+        /// <typeparam name="T">collection type</typeparam>
+        /// <param name="tasks">collection of tasks</param>
+        /// <returns>enumerable of type T</returns>
+        public static async Task<IEnumerable<T>> WhenAll<T>(params Task<T>[] tasks)
+        {
+            var allTasks = Task.WhenAll(tasks);
+
+            try
+            {
+                return await allTasks;
+            }
+            catch (Exception)
+            {
+                // ignore
+            }
+
+            throw allTasks.Exception ?? throw new Exception();
+        }
+    }
+}
