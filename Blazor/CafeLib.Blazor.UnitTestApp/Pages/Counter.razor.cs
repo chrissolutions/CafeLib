4ffb1d11cd006c1f1bfd180d1ded8488235369c1
chrissolutions <chris@chrissolutions.com>
2022-05-09 15:39:23 +1200

    Converted Counter page to use code behind.

---
+++ Blazor/CafeLib.Blazor.UnitTestApp/Pages/Counter.razor.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace CafeLib.Blazor.UnitTestApp.Pages
+{
+    public partial class Counter : ComponentBase
+    {
+        private int _currentCount = 0;
+
+        private void IncrementCount()
+        {
+            _currentCount++;
+        }
+    }
+}
