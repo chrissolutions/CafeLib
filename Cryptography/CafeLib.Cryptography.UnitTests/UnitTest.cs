c866619eb024e8a0238ac4c0eafefccfc086431d
chrissolutions <chris@chrissolutions.com>
2022-08-07 14:03:22 +1200

    Clarify ChecksumSize in Base58CheckEncoder

--- Cryptography/CafeLib.Cryptography.UnitTests/UnitTest.cs
+++ Cryptography/CafeLib.Cryptography.UnitTests/UnitTest.cs
@@ -1,18 +1,11 @@
 using System;
 using CafeLib.Cryptography.UnitTests.BsvSharp.Network;
-using Xunit;
 
 namespace CafeLib.Cryptography.UnitTests
 {
     public class UnitTest
     {
-        private static readonly Lazy<IBitcoinNetwork> LazyNetwork = new Lazy<IBitcoinNetwork>(() => new MainNetwork(), true);
+        private static readonly Lazy<IBitcoinNetwork> LazyNetwork = new(() => new MainNetwork(), true);
         public static readonly IBitcoinNetwork Network = LazyNetwork.Value;
-
-        [Fact]
-        public void Test1()
-        {
-
-        }
     }
 }
--- Cryptography/CafeLib.Cryptography/Base58CheckEncoder.cs
+++ Cryptography/CafeLib.Cryptography/Base58CheckEncoder.cs
@@ -6,10 +6,12 @@ namespace CafeLib.Cryptography
 {
     public class Base58CheckEncoder : IEncoder
     {
+        private const int ChecksumSize = 4;
         private static readonly Base58Encoder Base58Encoder = new();
 
+
         /// <summary>
-        /// Appends first 4 bytes of double SHA256 hash to bytes before standard Base58 encoding.
+        /// Appends first 4 bytes of double SHA256 hash as checksum to bytes before standard Base58 encoding.
         /// </summary>
         /// <param name="bytes"></param>
         /// <returns></returns>
@@ -17,9 +19,9 @@ namespace CafeLib.Cryptography
         {
             var span = new ReadOnlyByteSpan(bytes);
             var checksum = span.Hash256();
-            var buf = new byte[span.Length + 4];
+            var buf = new byte[span.Length + ChecksumSize];
             span.CopyTo(buf);
-            checksum.Span[..4].CopyTo(buf.AsSpan()[span.Length..]);
+            checksum.Span[..ChecksumSize].CopyTo(buf.AsSpan()[span.Length..]);
             return Base58Encoder.Encode(buf);
         }
 
@@ -34,10 +36,10 @@ namespace CafeLib.Cryptography
         {
             if (!Base58Encoder.TryDecode(encoded, out bytes)) return false;
             var span = bytes.AsSpan();
-            var checksum = span[^4..];
-            bytes = span[..^4].ToArray();
+            var checksum = span[^ChecksumSize..];
+            bytes = span[..^ChecksumSize].ToArray();
             var hash = ((ReadOnlyByteSpan)bytes).Hash256();
-            return checksum.SequenceEqual(hash.Span[..4]);
+            return checksum.SequenceEqual(hash.Span[..ChecksumSize]);
         }
     }
 }
