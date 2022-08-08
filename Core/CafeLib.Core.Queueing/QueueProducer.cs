d42df01a6375f1ac2ea197cad503540f4c7282a3
chrissolutions <chris@chrissolutions.com>
2022-08-08 12:19:11 +1200

    Added call to Task.CompletedTask in QueueProducer.cs when stopping.

--- Core/CafeLib.Core.Queueing/QueueProducer.cs
+++ Core/CafeLib.Core.Queueing/QueueProducer.cs
@@ -131,6 +131,7 @@ namespace CafeLib.Core.Queueing
             }
 
             await base.Stop().ConfigureAwait(false);
+            await Task.CompletedTask.ConfigureAwait(false);
         }
 
         #endregion
