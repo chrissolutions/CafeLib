55f0d2a9fac87dadcbc06e67afee69537dfd26ec
chrissolutions <chris@chrissolutions.com>
2022-08-22 17:35:45 +1200

    Removed Maui

--- Samples/Maui/HelloMaui/HelloMaui/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HelloMaui;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- Samples/Maui/HelloMaui/HelloMaui/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace HelloMaui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
